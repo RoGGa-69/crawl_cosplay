352d8809e8e1973c6386e5c9db73508991031540
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-07-05 09:53:15 -0400

    Update home.html.php
    echo "<br>";

--- content/pages/tournament/home.html.php
+++ content/pages/tournament/home.html.php
@@ -107,6 +107,8 @@ if ($active) :
 <?php else : // no active challenge
 	// echo $this->part('pause');
 	echo "<h3>There is currently no active Crawl Cosplay Trunk Tournament (CCTT)</h3>";
+	echo "<br>";
 	echo 'For the last tournament result, see: <a href="/tournament/tresults.html?set=32">CCTT#1 results</a>.';
+	echo "<br>";
 endif;
 ?>
