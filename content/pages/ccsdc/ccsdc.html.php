a7087b8d878ba24c759e737fd9cb95b656232339
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-09-28 14:09:24 -0400

    Create ccsdc.html.php

---
+++ content/pages/ccsdc/ccsdc.html.php
@@ -0,0 +1,32 @@
+<?php $this->layout = 'ccsdc'; ?>
+<h2 style="color:rgb(69, 136, 5);">About Crawl Cosplay Sudden Death Challenges (CCSDC)</h2>
+
+<p>The Crawl Cosplay Sudden Death Challenges is a competition that aims to fill a
+Crawl niche not currently filled by the biannual version release tournaments or other Crawl Cosplays.
+The idea is for players to compete by trying to do as well as possible in a
+game of Crawl with one attempt only; if you die, that challenge is over (thus
+"sudden death", though you may&mdash;<em>will</em>&mdash;also die suddenly). This competition is
+a lower time commitment event that aims to challenge players while playing one of the 100 or so Crawl Unique characters.</p>
+
+<h2>Competition Format</h2>
+<ul>
+<li>Each challenge consists of playing a Crawl Unique combo.</li>
+<li>You get <em>one</em>  attempt to play each combo.</li>
+<li>The goal is to advance as far as possible (and win!) in each game, scoring
+points by reaching various in-game milestones.</li>
+<li>Only games played and milestones scored between 00:00 UTC on the start and end dates count.</li>
+<li>Details on rules and scoring are available on the <a href="rules.html">rules page</a>.</li>
+</ul>
+
+<h2>Schedule</h2>
+
+<ul id="schedule">
+  <li><span class="label"><a href="1.html">Week 1</a>:</span> 2024-11-01 to 2024-11-07 &mdash; Agnes, a SpFi
+  <li><span class="label"><a href="2.html">Week 2</a>:</span> 2024-11-08 to 2024-11-14 &mdash; Amaemon, a DsAl
+  <li><span class="label"><a href="3.html">Week 3</a>:</span> 2024-11-15 to 2024-11-21 &mdash; Asterion, a Mimo
+  <li><span class="label"><a href="4.html">Week 4</a>:</span> 2024-11-22 to 2024-11-28 &mdash; Azrael, a DjFE
+  <li><span class="label"><a href="5.html">Week 5</a>:</span> 2024-11-29 to 2024-12-05 &mdash; Bai Suzhen, a DrSh
+</ul>
+
+<h2>Sign Up</h2> 
+<p>Sign ups are now closed.</p>
