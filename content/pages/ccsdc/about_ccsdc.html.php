81a5f7af7ec129b8116cdb8bddea27837fed240b
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-09-28 13:18:47 -0400

    Update about_ccsdc.html.php

--- content/pages/ccsdc/about_ccsdc.html.php
+++ content/pages/ccsdc/about_ccsdc.html.php
@@ -2,30 +2,39 @@
 <h2 style="color:rgb(69, 136, 5);">About Crawl Cosplay Sudden Death Challenges (CCSDC)</h2>
 
 <br>
-The Crawl Cosplay Sudden Death Challenges is a competition that aims to fill a Crawl niche not currently filled by the biannual version release tournaments. The idea is for players to compete by trying to do as well as possible in a game of Crawl with one attempt only; if you die, that challenge is over (thus "sudden death", though you may—will—also die suddenly). This competition is a lower time commitment event that aims to challenge players while simultaneously encouraging unusual characters and play styles that you might not normally consider.
+<p>The Crawl Cosplay Sudden Death Challenges is a competition that aims to fill a
+Crawl niche not currently filled by the biannual version release tournaments or other Crawl Cosplays.
+The idea is for players to compete by trying to do as well as possible in a
+game of Crawl with one attempt only; if you die, that challenge is over (thus
+"sudden death", though you may&mdash;<em>will</em>&mdash;also die suddenly). This competition is
+a lower time commitment event that aims to challenge players while playing one of the 100 or so Crawl Unique characters.</p>
 
-Milestones from CSDC games will be announced in the IRC channel ##csdc on freenode. It's a great place to hang out if you want to live spectate ongoing games or talk with other people about the competition.
+<h2>Competition Format</h2>
+<ul>
+<li>Each challenge consists of playing a Crawl Unique combo.</li>
+<li>You get <em>one</em>  attempt to play each combo.</li>
+<li>The goal is to advance as far as possible (and win!) in each game, scoring
+points by reaching various in-game milestones.</li>
+<li>Only games played and milestones scored between 00:00 UTC on the start and end dates count.</li>
+<li>Details on rules and scoring are available on the <a href="rules.html">rules page</a>.</li>
+</ul>
 
-Competition Format
-Each challenge consists of playing a randomly chosen Crawl combo.
-You get one attempt to play each combo.
-The goal is to advance as far as possible (and win!) in each game, scoring points by reaching various in-game milestones.
-Only games played and milestones scored between 00:00 UTC on the start and end dates count.
-Details on rules and scoring are available on the rules page.
-Schedule
-Week 1—HuWn: 2018-10-04 to 2018-10-11
-Week 2—TrCj: 2018-10-11 to 2018-10-18
-Week 3—BaHu: 2018-10-18 to 2018-10-25
-Week 4—DgSk: 2018-10-25 to 2018-11-01
-Week 5—OpMo: 2018-11-01 to 2018-11-08
-Week 6—SpNe: 2018-11-08 to 2018-11-15
-Week 7—GhWr: 2018-11-15 to 2018-11-22
-Cool Plays
-If you do something particularly cool in your game make a note cool play with the : command. Follow it with a second note indicating where the play started and what makes it cool. The CSDC Replay team will look through cool plays and post highlight reels from each week.
+<h2>Schedule</h2>
 
-Week 1: Cool Plays, Sudden Deaths, Highlights (coming soon!)
-Sign Up
-Sign ups are now closed. See you in 0.23.
+<ul id="schedule">
+  <li><span class="label"><a href="1.html">Week 1&mdash;HuWn</a>:</span> 2018-10-04 to 2018-10-11
+  <li><span class="label"><a href="2.html">Week 2&mdash;TrCj</a>:</span> 2018-10-11 to 2018-10-18
+  <li><span class="label"><a href="3.html">Week 3&mdash;BaHu</a>:</span> 2018-10-18 to 2018-10-25
+  <li><span class="label"><a href="4.html">Week 4&mdash;DgSk</a>:</span> 2018-10-25 to 2018-11-01
+  <li><span class="label"><a href="5.html">Week 5&mdash;OpMo</a>:</span> 2018-11-01 to 2018-11-08
+  <li><span class="label"><a href="6.html">Week 6&mdash;SpNe</a>:</span> 2018-11-08 to 2018-11-15
+  <li><span class="label"><a href="7.html">Week 7&mdash;GhWr</a>:</span> 2018-11-15 to 2018-11-22
+</ul>
 
-Credits
-Original CSDC rules and organization by WalkerBoh. Postquell IRC support by Kramin. The replay team is led by mibe. Dungeon Crawl Stone Soup by the Stone Soup Team. Thank you to all the players who made runs in the beta. I am your host, ebering.
+<h2>Sign Up</h2> <p>Sign ups are now closed. See you in 0.23.</p>
+
+<h2>Credits</h2>
+
+<p>Original CSDC rules and organization by <a href="http://crawl.akrasiac.org/scoring/players/walkerboh.html">WalkerBoh</a> 
+and <a href="http://crawl.akrasiac.org/scoring/players/ebering.html">ebering</a>.
+Thank you to all the players who made runs in the beta. I am your host, RoGGa.
