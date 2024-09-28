017210c4a3f9e765fae2e0d6a5b25f7c81f017c4
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-09-28 13:25:52 -0400

    Update about_ccsdc.html.php

--- content/pages/ccsdc/about_ccsdc.html.php
+++ content/pages/ccsdc/about_ccsdc.html.php
@@ -32,6 +32,84 @@ points by reaching various in-game milestones.</li>
 
 <h2>Sign Up</h2> <p>Sign ups are now closed. See you in 0.23.</p>
 
+    <h2>Rules</h2></div><div id="content">
+    <ol>
+<li>Each challenge consists of playing a randomly chosen Crawl race/class combo
+(e.g. MiBe). The combo for each week of the competition will be announced at
+00:00 UTC on the Thursday starting that week. All players have one week to
+finish a game using that combo. Only milestones recorded during the week (from
+00:00 UTC on the start date until 00:00 UTC on the end date) will count for
+scoring.</li>
+<li>Your first 0.22 game started on an official server during the week will count
+for scoring. This is the only allowed attempt, subject to rule 3 below.</li>
+<li>One redo per week is allowed if your first game ended in death with player
+XL < 5 (i.e., no redo once you hit XL 5). The redo game must be started after
+the end of the first game (no startscumming!).  The highest CSDC score of the
+two games is counted towards your score.</li>
+<li>Each challenge has an optional bonus challenge in addition to the
+race/class combo. Each bonus will have two tiers; the second tier is more
+difficult and worth more points.</li>
+<li>Each challenge includes a list of gods. A bonus point can be earned upon
+reaching ****** piety (or on worship with Gozag or Xom) with one of the listed
+gods. The point is lost if you ever abandon your god or are excommunicated. If
+the combo for the week is a zealot background, god points are only for sticking
+with the starting god. If the combo for the week is a demigod, the god point is automatically awarded.</li>
+<li>The season consists of 7 challenges total (i.e., 7 different combos). Each
+race and background will be selected at most once during the competition.</li>
+<li>The final rankings will be tallied at the end of week 7 and represent the
+final outcome. The player with the highest CSDC score is the champion.</li>
+<li>Tiebreakers are (in order): number of weekly bonus points, highest in-game
+score.</li>
+<li>Don't play on more than one account. That misses the point.</li>
+</ol>
+
+<h2>Scoring</h2>
+
+<p>Scoring is divided into two parts, weekly points assigned to each game
+played, and one-time points awarded once per season regardless of how many
+games achieve them.</p>
+
+<table class="info"><tr class="head"><th>Weekly points (can be earned each
+week)</th><th></th></tr>
+<tr><td class="name">Kill a unique:</td><td class="pt">1</td></tr>
+<tr><td class="name">Enter a multi-level branch of the dungeon:</td><td class="pt">1</td></tr>
+<tr><td class="name">Reach the end of a multi-level branch (including D):</td><td class="pt">1</td></tr>
+<tr><td class="name">Champion a listed god:</td><td class="pt">1</td></tr>
+<tr><td class="name">Collect a rune:</td><td class="pt">1</td></tr>
+<tr><td class="name">Collect 3 or more runes:</td><td class="pt">1</td></tr>
+<tr><td class="name">Win:</td><td class="pt">1</td></tr>
+<tr><td class="name">Complete a Tier I bonus:</td><td class="pt">1</td></tr>
+<tr><td class="name">Complete a Tier II bonus:</td><td class="pt">2</td></tr>
+<tr class="head" id="onetime"><th>One-time points (earned once in the
+competition)</th><th></th></tr>
+<tr><td class="name">Win a game with 15 runes:</td><td class="pt">3</td></tr>
+<tr><td class="name">Win a game in under 50,000 turns:</td><td class="pt">3</td></tr>
+<tr><td class="name">Clear a Ziggurat:</td><td class="pt">3</td></tr>
+<tr><td class="name">Enter Zot at XL 20 or lower:</td><td class="pt">6</td></tr>
+<tr><td class="name">Win a game without entering lair:</td><td class="pt">6</td></tr>
+<tr><td class="name">Get a rune without using potions or scrolls:</td><td class="pt">6</td></tr>
+</table>
+
+<p class="notes"> It will not always be the case that earning a
+Tier II bonus implies that you have earned a Tier I bonus, these points can be
+awarded separately. Unless specified, a bonus or one time point does not
+require you to win to earn the point.</p>
+
+<h2>Fair Crawling</h2>
+
+<p>All contestants will compete with commonly recognized principles of
+sportsmanship and fair play. Failure to do so will result in disqualification
+without recourse and your entry will be deleted from the scoring database. If
+you are uncertain if something is "fair play" then ask for clarification before
+doing it. Ignorance and negligence are not excuses for poor behavior.</p>
+
+<p>All contestants acknowledge an obligation not to commit misconduct in
+relation to their participation.  Misconduct is any act that is a breach of
+good manners, a breach of good sportsmanship, or unethical behavior. Misconduct
+will also result in disqualification without recourse and your entry will be
+deleted from the scoring database. Severe misconduct will lead to exclusion from
+future CSDC events.</p>
+
 <h2>Credits</h2>
 
 <p>Original CSDC rules and organization by <a href="http://crawl.akrasiac.org/scoring/players/walkerboh.html">WalkerBoh</a> 
