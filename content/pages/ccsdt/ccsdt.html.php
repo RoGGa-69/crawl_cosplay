ee9d384471ab731b5bab03e13c6ca6462479b78a
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-09-28 15:13:30 -0400

    Update ccsdt.html.php

--- content/pages/ccsdt/ccsdt.html.php
+++ content/pages/ccsdt/ccsdt.html.php
@@ -14,7 +14,7 @@
 <ul id="schedule">
   <li><span class="label"><a href="1.html">Week 1</a> :</span> 2024-11-01 to 2024-11-07 &mdash; Donald, a HuFi
   <li><span class="label"><a href="2.html">Week 2</a> :</span> 2024-11-08 to 2024-11-14 &mdash; Amaemon, a DsAl
-  <li><span class="label"><a href="3.html">Week 3</a> :</span> 2024-11-15 to 2024-11-21 &mdash; Asterion, a Mimo
+  <li><span class="label"><a href="3.html">Week 3</a> :</span> 2024-11-15 to 2024-11-21 &mdash; Asterion, a MiMo
   <li><span class="label"><a href="4.html">Week 4</a> :</span> 2024-11-22 to 2024-11-28 &mdash; Azrael, a DjFE
   <li><span class="label"><a href="5.html">Week 5</a> :</span> 2024-11-29 to 2024-12-05 &mdash; Bai Suzhen, a DrSh
 </ul>
