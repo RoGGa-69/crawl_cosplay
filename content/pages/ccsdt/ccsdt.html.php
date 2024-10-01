aa9150a97d16a0789b26f6606f534d4b2f6c0bcb
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-10-01 10:43:07 -0400

    Update ccsdt.html.php

--- content/pages/ccsdt/ccsdt.html.php
+++ content/pages/ccsdt/ccsdt.html.php
@@ -12,17 +12,16 @@
 <h2>Schedule</h2>
 
 <ul id="schedule">
-  <li><span class="label"><a href="1.html">Week 1</a> :</span> 2024-11-01 to 2024-11-07 &mdash; Frances, a Human Gladiator (HuGl)
-  <li><span class="label"><a href="2.html">Week 2</a> :</span> 2024-11-08 to 2024-11-14 &mdash; Sojobo, a Tengu Air Elementalist (TeAE)
-  <li><span class="label"><a href="3.html">Week 3</a> :</span> 2024-11-15 to 2024-11-21 &mdash; The Enchantress, a Spriggan	Enchanter (SpEn)
-  <li><span class="label"><a href="4.html">Week 4</a> :</span> 2024-11-22 to 2024-11-28 &mdash; Natasha, a Felid Necromancer (FeNe)
-  <li><span class="label"><a href="5.html">Week 5</a> :</span> 2024-11-29 to 2024-12-05 &mdash; Amaemon, a Demonspawn	Alchemist (DsAl)
+  <li><span class="label"><a href="/0.32/1.html">Week 1</a> :</span> 2024-11-01 to 2024-11-07 &mdash; Frances, a Human Gladiator (HuGl)
+  <li><span class="label"><a href="/0.32/2.html">Week 2</a> :</span> 2024-11-08 to 2024-11-14 &mdash; Sojobo, a Tengu Air Elementalist (TeAE)
+  <li><span class="label"><a href="/0.32/3.html">Week 3</a> :</span> 2024-11-15 to 2024-11-21 &mdash; The Enchantress, a Spriggan	Enchanter (SpEn)
+  <li><span class="label"><a href="/0.32/4.html">Week 4</a> :</span> 2024-11-22 to 2024-11-28 &mdash; Natasha, a Felid Necromancer (FeNe)
+  <li><span class="label"><a href="/0.32/5.html">Week 5</a> :</span> 2024-11-29 to 2024-12-05 &mdash; Amaemon, a Demonspawn	Alchemist (DsAl)
 </ul>
 
 <h2>Sign Up</h2> 
-<p>In order to sign up, set the first line of your 0.32 rcfile to</p> <pre
-id="rc"># ccsdt</pre><p>on <a
-href="https://crawl.develz.org/play.htm">any of the official online servers</a>
+<p>In order to sign up, set the first line of your 0.32 rcfile to</p> <pre id="rc"># ccsdt</pre><p>
+  on <a href="https://crawl.develz.org/play.htm">any of the official online servers</a>
 before the end of the first week. Your name will appear in the standings once
 you've done this correctly and started at least one 0.32 game (though it may take about 20 minutes before it does).</p>
 
