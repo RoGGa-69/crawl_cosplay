ad190b6acad9b1b29afb452e43b0a4e5cec5014c
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-10-01 12:15:39 -0400

    Update head.html.php

--- content/layouts/ccsdt/head.html.php
+++ content/layouts/ccsdt/head.html.php
@@ -7,6 +7,181 @@
     <link rel="stylesheet" href="/css/ccsdt.css?v=<?=time()?>">
     <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
 
+<style>
+
+  html, body {
+    margin: 0;
+    background-color: #202;
+    color: #ccc;
+    font-family: sans-serif;
+}
+
+#logo {
+	display: block;
+	margin-left: auto;
+	margin-right: auto;
+}
+
+#sdc {
+	margin-top: -40px;
+	font-variant: small-caps;
+	font-size: 36pt;
+	margin-bottom: -20px;
+}
+#title {
+    width: 100%;
+    background-color: #202;
+    color: #eb2;
+    text-align: center;
+}
+
+#content {
+    margin-bottom: 40px;
+    margin-left: auto;
+    margin-right: auto;
+    width: calc(100% - 20px);
+}
+
+a {
+    color: #ccc;
+}
+h2 {
+	text-align: center;
+}
+p,ul,ol,.info {
+	margin-left: calc(20% - 10px);
+	margin-right: auto;
+	width: 60%;
+	padding: 0;
+}
+
+pre {
+	background: #606;
+	border: 3px dashed #669;
+	margin-left: auto;
+	margin-right: auto;
+	padding: 10px;
+}
+
+#rc {
+	width: 6em;
+	font-size: 12pt;
+	text-align: center;
+}
+
+#cover {
+	width: 45em;
+	overflow-x: hidden;
+	overflow-y: hidden;
+}
+
+li {
+	margin-left: auto;
+	margin-right: auto;
+	width: 80%;
+}
+#combo, #bonus, #gods, #times, #schedule  {
+    font-size: 14pt;
+}
+
+.label {
+	font-weight: bold;
+}
+
+.card {
+	overflow-x: scroll;
+    margin-left: 20px;
+    margin-right: 20px;
+}
+
+table {
+    margin-top: 30px;
+    width: calc(100% - 60px);
+    margin-left: auto;
+    margin-right: auto;
+    border-collapse: collapse;
+}
+td.pt, td.total {
+	text-align: center;
+}
+td, th {
+	padding-left: 10px;
+	padding-right: 10px;
+	padding-bottom: 5px;
+	padding-top: 5px;
+}
+
+th {
+	vertical-align: top;
+}
+
+tr.head {
+	font-size: 14pt;
+	border-bottom: 3px solid #669;
+	border-top: 3px solid #669;
+}
+
+tr {
+	background-color: #606;
+}
+tr:nth-child(even) {
+	background-color: #303;
+}
+
+tr.won {
+  background-color: #363;
+  font-weight: bold;
+}
+tr.won:nth-child(even) {
+  background-color: #002A00;
+}
+tr.dead {
+  background-color: #711;
+}
+tr.dead:nth-child(even) {
+  background-color: #450000;
+}
+tr.alive {background-color: #383338}
+tr.alive:nth-child(even) {background-color: #1F1A1F;}
+
+tr.none {
+	color: #477;
+	background-color: #111;
+}
+
+tr.none:nth-child(even) {background-color: #000;}
+
+.menu {
+	padding-left: 10px;
+}
+
+img.flag {
+    vertical-align: middle;
+    padding-right: 8px;
+}
+
+.menuspacer {
+	margin-left: 10px;
+	border-left: 3px solid #ccc;
+	height: calc(14pt);
+}
+
+#bottomtext {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #606;
+    padding: 5px;
+}
+
+#updated {
+	float: right;
+	margin-right: 15px;
+}
+
+</style>
+    
 <?php
 
     if (isset($meta) && is_array($meta) && isset($meta['filename'])) {
