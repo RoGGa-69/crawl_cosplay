6e53d085140c468ddfc27096c970519ad9e28c81
RoGGa <robert_gagne69@yahoo.com>
2024-10-01 11:02:06 -0400

    dmp

--- content/pages/ccsdt/0.32/index.html.php
+++ content/pages/ccsdt/0.32/index.html.php
@@ -1,6 +1,42 @@
-<div id="content"><head><title>Crawl Cosplay Sudden Death Tournament</title>
+<head><title>Crawl Cosplay Sudden Death Tournament</title>
     <link rel="stylesheet" href="static/score.css">
-    </head></div>
-    <div id="bottomtext"><div id="title">
+    </head><div id="title">
     <h1 id="sdc">0.32 Sudden Death Tournament</h1>
-    <h2></h2></div></div>
+    <h2></h2></div><div id="content">
+    <pre id="cover">
+_You are suddenly pulled into a different region of the Abyss!
+_A floating eye, a glowing orange brain, 4 small abominations and 8 large
+ abominations come into view.</pre>
+<p>The Crawl Cosplay Sudden Death Tournament is a competition that aims to fill a
+Crawl niche not currently filled by the biannual version release tournaments.
+The idea is for players to compete by trying to do as well as possible in a
+game of Crawl with one attempt only; if you die, that challenge is over (thus
+"sudden death", though you may&mdash;<em>will</em>&mdash;also die suddenly). This competition is
+a lower time commitment event that aims to challenge players while
+simultaneously encouraging unusual characters and play styles that you might
+not normally consider.</p>
+
+<h2>Competition Format</h2>
+
+<ul>
+<li>Each challenge consists of playing a randomly chosen Crawl combo.</li>
+<li>You get <em>one</em>  attempt to play each combo.</li>
+<li>The goal is to advance as far as possible (and win!) in each game, scoring
+points by reaching various in-game milestones.</li>
+<li>Only games played and milestones scored between 00:00 UTC on the start and end dates count.</li>
+<li>Details on rules and scoring are available on the <a
+href="rules.html.php">rules page</a>.</li>
+</ul>
+
+<h2>Schedule</h2>
+
+<ul id="schedule"><li><span class="label"><a href="1.html">Week 1&mdash;AtHu</a>:</span> 2024-09-23 to 2024-09-30<li><span class="label"><a href="2.html">Week 2&mdash;OpSu</a>:</span> 2024-09-30 to 2024-10-07<li><span class="label">Week 3:</span> 2024-10-07 to 2024-10-14<li><span class="label">Week 4:</span> 2024-10-14 to 2024-10-21<li><span class="label">Week 5:</span> 2024-10-21 to 2024-10-28</ul>
+
+<h2>Sign Up</h2> <p>Sign ups are now closed. See you in 0.33.</p>
+
+<h2>Credits</h2>
+
+<p>Original CSDC rules and organization by <a href="http://crawl.akrasiac.org/scoring/players/walkerboh.html">WalkerBoh</a> and 
+<a href="http://crawl.akrasiac.org/scoring/players/ebering.html">ebering</a>.
+Thank you to scrubbdaddy for python assistance. I am your host, RoGGa.</div>
+    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a></span><span class="menu"><a href="rules.html">Rules</a></span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a></span><span class="menu"><a href="2.html">Week 2</a></span><span class="menu">Week 3</span><span class="menu">Week 4</span><span class="menu">Week 5</span></div>
--- content/pages/ccsdt/0.32/rules.html.php
+++ content/pages/ccsdt/0.32/rules.html.php
@@ -1,6 +1,69 @@
-<div id="content"><head><title>Rules</title>
+<head><title>Rules</title>
     <link rel="stylesheet" href="static/score.css">
-    </head></div>
-    <div id="bottomtext"><div id="title">
+    </head><div id="title">
     <h1 id="sdc">0.32 Sudden Death Tournament</h1>
-    <h2>Rules</h2></div></div>
+    <h2>Rules</h2></div><div id="content">
+    <ol>
+<li>Each challenge consists of playing a randomly chosen Crawl race/class combo
+(e.g. MiBe). The combo for each week of the competition will be announced at
+00:00 UTC on the Friday starting that week. All players have one week to
+finish a game using that combo. Only milestones recorded during the week (from
+00:00 UTC on the start date until 00:00 UTC on the end date) will count for
+scoring.</li>
+<li>Your first 0.22 game started on an official server during the week will count
+for scoring. This is the only allowed attempt, subject to rule 3 below.</li>
+<li>One redo per week is allowed if your first game ended in death with player
+XL < 5 (i.e., no redo once you hit XL 5). The redo game must be started after
+the end of the first game (no startscumming!).  The highest CSDC score of the
+two games is counted towards your score.</li>
+<li>Each challenge has an optional bonus challenge in addition to the
+race/class combo. Each bonus will have two tiers; the second tier is more
+difficult and worth more points.</li>
+<li>Each challenge includes a list of gods. A bonus point can be earned upon
+reaching ****** piety (or on worship with Gozag or Xom) with one of the listed
+gods. The point is lost if you ever abandon your god or are excommunicated. If
+the combo for the week is a zealot background, god points are only for sticking
+with the starting god. If the combo for the week is a demigod, the god point is automatically awarded.</li>
+<li>The season consists of 5 challenges total (i.e., 5 different combos). Each
+race and background will be selected at most once during the competition.</li>
+<li>The final rankings will be tallied at the end of week 5 and represent the
+final outcome. The player with the highest CSDC score is the champion.</li>
+<li>Tiebreakers are (in order): number of weekly bonus points, highest in-game
+score.</li>
+<li>Don't play on more than one account. That misses the point.</li>
+</ol>
+
+<h2>Scoring</h2>
+
+<p>Scoring is divided into two parts, weekly points assigned to each game
+played, and one-time points awarded once per season regardless of how many
+games achieve them.</p>
+
+<table class="info"><tr class="head"><th>Weekly points (can be earned each
+week)</th><th></th></tr>
+<tr><td class="name">Kill a unique:</td><td class="pt">1</td></tr>
+<tr><td class="name">Enter a multi-level branch of the dungeon:</td><td class="pt">1</td></tr>
+<tr><td class="name">Reach the end of a multi-level branch (including D):</td><td class="pt">1</td></tr>
+<tr><td class="name">Champion a listed god:</td><td class="pt">1</td></tr>
+<tr><td class="name">Collect a rune:</td><td class="pt">1</td></tr>
+<tr><td class="name">Collect 3 or more runes:</td><td class="pt">1</td></tr>
+<tr><td class="name">Collect the Orb:</td><td class="pt">1</td></tr>
+<tr><td class="name">Win:</td><td class="pt">1</td></tr>
+<tr><td class="name">Complete a Tier I bonus:</td><td class="pt">1</td></tr>
+<tr><td class="name">Complete a Tier II bonus:</td><td class="pt">1</td></tr>
+<tr class="head" id="onetime"><th>One-time points (earned once in the competition)</th><th></th></tr>
+<tr><td class="name">Win a game with 15 runes:</td><td class="pt">3</td></tr>
+<tr><td class="name">Clear a Ziggurat:</td><td class="pt">3</td></tr>
+<tr><td class="name">Enter Zot at XL 20 or lower:</td><td class="pt">3</td></tr>
+<tr><td class="name">Win a game in under 40,000 turns:</td><td class="pt">6</td></tr>
+<tr><td class="name">Win a game without entering lair:</td><td class="pt">6</td></tr>
+<tr><td class="name">Get a rune without using potions or scrolls:</td><td class="pt">6</td></tr>
+</table>
+
+<p class="notes"> It will not always be the case that earning a
+Tier II bonus implies that you have earned a Tier I bonus, these points can be
+awarded separately. Unless specified, a bonus or one time point does not
+require you to win to earn the point.</p>
+
+</div>
+    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a></span><span class="menu"><a href="rules.html">Rules</a></span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a></span><span class="menu"><a href="2.html">Week 2</a></span><span class="menu">Week 3</span><span class="menu">Week 4</span><span class="menu">Week 5</span></div>
