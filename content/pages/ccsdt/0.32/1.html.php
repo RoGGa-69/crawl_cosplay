3c5a978b97348a416ae849eaedfce074a4e3154f
RoGGa <robert_gagne69@yahoo.com>
2024-10-01 10:31:14 -0400

    dump

--- content/pages/ccsdt/0.32/1.html.php
+++ content/pages/ccsdt/0.32/1.html.php
@@ -7,17 +7,17 @@
 <span class="label">Tier II Bonus: </span>Reach the end of the Vaults at XL &leq; 18.</div>
 <div id="gods"><span class="label">Gods: </span>Ashenzari, Ashenzari, Ashenzari</div><div class="card"><table><tr class="head">
     <th>Player</th>
-    <th>| Unique Kill</th>
-    <th>| Branch Enter</th>
-    <th>| Branch End</th>
-    <th>| Champion God</th>
-    <th>| Collect a Rune</th>
-    <th>| Collect 3 Runes</th>
-    <th>| Obtain the Orb</th>
-    <th>| Win</th>
-    <th>| Bonus I</th>
-    <th>| Bonus II</th>
-    <th>| Total</th>
+    <th>Unique Kill</th>
+    <th>Branch Enter</th>
+    <th>Branch End</th>
+    <th>Champion God</th>
+    <th>Collect a Rune</th>
+    <th>Collect 3 Runes</th>
+    <th>Obtain the Orb</th>
+    <th>Win</th>
+    <th>Bonus I</th>
+    <th>Bonus II</th>
+    <th>Total</th>
     </tr><tr class="none"><td class="name">cfcfcfcfcf</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Colgate</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">HourglassMan</td>
@@ -27,4 +27,4 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGa</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Blyxx</td>
                 <td colspan="10"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a></span><span class="menu"><a href="rules.html">Rules</a></span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1</span><span class="menu"><a href="2.html">Week 2</a></span><span class="menu">Week 3</span><span class="menu">Week 4</span><span class="menu">Week 5</span><span id="updated"><span class="label">Updated: </span>2024-10-01 14:29 UTC</span></div></div></body></html>
+    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a></span><span class="menu"><a href="rules.html">Rules</a></span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1</span><span class="menu"><a href="2.html">Week 2</a></span><span class="menu">Week 3</span><span class="menu">Week 4</span><span class="menu">Week 5</span><span id="updated"><span class="label">Updated: </span>2024-10-01 14:31 UTC</span></div></div></body></html>
--- content/pages/ccsdt/0.32/2.html.php
+++ content/pages/ccsdt/0.32/2.html.php
@@ -7,19 +7,19 @@
 <span class="label">Tier II Bonus: </span>Collect a rune with all base skills < 11.</div>
 <div id="gods"><span class="label">Gods: </span>Cheibriados, Cheibriados, Cheibriados</div><div class="card"><table><tr class="head">
     <th>Player</th>
-    <th>| Unique Kill</th>
-    <th>| Branch Enter</th>
-    <th>| Branch End</th>
-    <th>| Champion God</th>
-    <th>| Collect a Rune</th>
-    <th>| Collect 3 Runes</th>
-    <th>| Obtain the Orb</th>
-    <th>| Win</th>
-    <th>| Bonus I</th>
-    <th>| Bonus II</th>
-    <th>| Total</th>
-    </tr><tr class="dead"><td class="name"><img src="static/cdi.png" class="flag" /><a href="https://crawl.dcss.io/crawl/morgue//RoGGa/morgue-RoGGa-20240930-144112.txt">RoGGa</a></td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
-<tr class="alive"><td class="name"><img src="static/cbro.png" class="flag" />Strat</td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
+    <th>Unique Kill</th>
+    <th>Branch Enter</th>
+    <th>Branch End</th>
+    <th>Champion God</th>
+    <th>Collect a Rune</th>
+    <th>Collect 3 Runes</th>
+    <th>Obtain the Orb</th>
+    <th>Win</th>
+    <th>Bonus I</th>
+    <th>Bonus II</th>
+    <th>Total</th>
+    </tr><tr class="dead"><td class="name"><img src="static/cbro.png" class="flag" /><a href="https://cbro.berotato.org/morgue//Strat/morgue-Strat-20241001-143021.txt">Strat</a></td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
+<tr class="dead"><td class="name"><img src="static/cdi.png" class="flag" /><a href="https://crawl.dcss.io/crawl/morgue//RoGGa/morgue-RoGGa-20240930-144112.txt">RoGGa</a></td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
 <tr class="none"><td class="name">cfcfcfcfcf</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Colgate</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">HourglassMan</td>
@@ -27,4 +27,4 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">PigKeeper</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Blyxx</td>
                 <td colspan="10"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a></span><span class="menu"><a href="rules.html">Rules</a></span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a></span><span class="menu">Week 2</span><span class="menu">Week 3</span><span class="menu">Week 4</span><span class="menu">Week 5</span><span id="updated"><span class="label">Updated: </span>2024-10-01 14:29 UTC</span></div></div></body></html>
+    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a></span><span class="menu"><a href="rules.html">Rules</a></span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a></span><span class="menu">Week 2</span><span class="menu">Week 3</span><span class="menu">Week 4</span><span class="menu">Week 5</span><span id="updated"><span class="label">Updated: </span>2024-10-01 14:31 UTC</span></div></div></body></html>
