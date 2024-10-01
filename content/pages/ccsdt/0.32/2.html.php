0a4cb21169fc41f9613207de3aafabc705a9d266
RoGGa <robert_gagne69@yahoo.com>
2024-10-01 10:49:31 -0400

    dmp

--- content/pages/ccsdt/0.32/2.html.php
+++ content/pages/ccsdt/0.32/2.html.php
@@ -18,8 +18,8 @@
     <th>Bonus I</th>
     <th>Bonus II</th>
     <th>Total</th>
-    </tr><tr class="dead"><td class="name"><img src="static/cbro.png" class="flag" /><a href="https://cbro.berotato.org/morgue//Strat/morgue-Strat-20241001-143021.txt">Strat</a></td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
-<tr class="dead"><td class="name"><img src="static/cdi.png" class="flag" /><a href="https://crawl.dcss.io/crawl/morgue//RoGGa/morgue-RoGGa-20240930-144112.txt">RoGGa</a></td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
+    </tr><tr class="dead"><td class="name"><a href="https://cbro.berotato.org/morgue//Strat/morgue-Strat-20241001-143021.txt">Strat</a></td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
+<tr class="dead"><td class="name"><a href="https://crawl.dcss.io/crawl/morgue//RoGGa/morgue-RoGGa-20240930-144112.txt">RoGGa</a></td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
 <tr class="none"><td class="name">cfcfcfcfcf</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Colgate</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">HourglassMan</td>
@@ -27,4 +27,4 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">PigKeeper</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Blyxx</td>
                 <td colspan="10"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a></span><span class="menu"><a href="rules.html">Rules</a></span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a></span><span class="menu">Week 2</span><span class="menu">Week 3</span><span class="menu">Week 4</span><span class="menu">Week 5</span><span id="updated"><span class="label">Updated: </span>2024-10-01 14:46 UTC</span></div></div></body></html>
+    <div id="bottomtext"><span class="menu"><a href="index.html">Overview</a></span><span class="menu"><a href="rules.html">Rules</a></span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a></span><span class="menu">Week 2</span><span class="menu">Week 3</span><span class="menu">Week 4</span><span class="menu">Week 5</span><span id="updated"><span class="label">Updated: </span>2024-10-01 14:49 UTC</span></div></div></body></html>
