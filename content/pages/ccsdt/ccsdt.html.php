70aacc94c7d6c739e3c0be37369497e352eb4346
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-10-05 09:51:55 -0400

    Update ccsdt.html.php

--- content/pages/ccsdt/ccsdt.html.php
+++ content/pages/ccsdt/ccsdt.html.php
@@ -34,11 +34,11 @@ See the <a href="about_ccsdt.html">CCSDT About page</a>.
    <div id="bottomtext">
      <span class="menu"><a href="ccsdt.html">Overview</a> - </span>
      <span class="menu"><a href="about_ccsdt.html">Details</a> - </span>
-     <span class="menu"><a href="/0.32/standings.html">Standings</a></span>
+     <span class="menu"><a href="./0.32/standings.html">Standings</a></span>
      <span class="menuspacer"></span>
-     <span class="menu"><a href="/0.32/1.html">Week 1</a> - </span>
-     <span class="menu"><a href="/0.32/2.html">Week 2</a> - </span>
-     <span class="menu"><a href="/0.32/3.html">Week 3</a> - </span>
-     <span class="menu"><a href="/0.32/4.html">Week 4</a> - </span>
-     <span class="menu"><a href="/0.32/5.html">Week 5</a></span>
+     <span class="menu"><a href="./0.32/1.html">Week 1</a> - </span>
+     <span class="menu"><a href="./0.32/2.html">Week 2</a> - </span>
+     <span class="menu"><a href="./0.32/3.html">Week 3</a> - </span>
+     <span class="menu"><a href="./0.32/4.html">Week 4</a> - </span>
+     <span class="menu"><a href="./0.32/5.html">Week 5</a></span>
    </div>
