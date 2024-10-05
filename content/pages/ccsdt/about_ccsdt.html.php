1e9b01832ff83d06ec4e3dad124e1e4baab8124d
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-10-05 09:08:30 -0400

    Update about_ccsdt.html.php

--- content/pages/ccsdt/about_ccsdt.html.php
+++ content/pages/ccsdt/about_ccsdt.html.php
@@ -76,3 +76,15 @@ require you to win to earn the point.</p>
 <p>Original CSDC rules and organization by <a href="http://crawl.akrasiac.org/scoring/players/walkerboh.html">WalkerBoh</a> 
 and <a href="http://crawl.akrasiac.org/scoring/players/ebering.html">ebering</a>.
 Thank you to all the players who made runs in the beta. I am your host, RoGGa.
+
+   <div id="bottomtext">
+     <span class="menu"><a href="../ccsdt.html">Overview</a> - </span>
+     <span class="menu"><a href="../about_ccsdt.html">Details</a> - </span>
+     <span class="menu"><a href="standings.html">Standings</a></span>
+     <span class="menuspacer"></span>
+     <span class="menu"><a href="/0.32/1.html">Week 1</a> - </span>
+     <span class="menu"><a href="/0.32/2.html">Week 2</a> - </span>
+     <span class="menu"><a href="/0.32/3.html">Week 3</a> - </span>
+     <span class="menu"><a href="/0.32/4.html">Week 4</a> - </span>
+     <span class="menu"><a href="/0.32/5.html">Week 5</a></span>
+   </div>
