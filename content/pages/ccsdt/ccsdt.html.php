bf6d6dab0cf9a53a02f0acaca97fef17792d981d
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-10-06 09:18:31 -0400

    Update ccsdt.html.php

--- content/pages/ccsdt/ccsdt.html.php
+++ content/pages/ccsdt/ccsdt.html.php
@@ -7,7 +7,7 @@
 <li>You get <em>one</em> attempt to play each combo.</li>
 <li>The goal is to advance as far as possible (and win!) in each game, scoring points by reaching various in-game milestones.</li>
 <li>Only games played and milestones scored between 00:00 UTC on the start and end dates count. 
-    To find out what time this means for your timezone, see <a href="https://dateful.com/convert/utc"></a> and select <b>24</b> and <b>DATE</b>.</li>
+    To find out what time this is for your timezone, see: <a href="https://dateful.com/convert/utc">dateful.com/convert/utc</a> and select <b>24</b> and <b>DATE</b>.</li>
 </ul>
 
 <h2>Schedule</h2>
