b8dfd0d272163d73d6741f277ba2bc28d82aaaa5
RoGGa <robert_gagne69@yahoo.com>
2024-10-07 07:25:31 -0400

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.32/3.html.php
+++ content/pages/ccsdt/0.32/3.html.php
@@ -1,15 +1,22 @@
-<?php $this->layout = 'ccsdt'; ?>
-
-<h3>This week hasn't started yet!</h3>
-
-   <div id="bottomtext">
-     <span class="menu"><a href="ccsdt.html">Overview</a> - </span>
-     <span class="menu"><a href="about_ccsdt.html">Details</a> - </span>
-     <span class="menu"><a href="./0.32/standings.html">Standings</a></span>
-     <span class="menuspacer"></span>
-     <span class="menu"><a href="./0.32/1.html">Week 1</a> - </span>
-     <span class="menu"><a href="./0.32/2.html">Week 2</a> - </span>
-     <span class="menu"><a href="./0.32/3.html">Week 3</a> - </span>
-     <span class="menu"><a href="./0.32/4.html">Week 4</a> - </span>
-     <span class="menu"><a href="./0.32/5.html">Week 5</a></span>
-   </div>
+<title>Week 3 &mdash; MfFi</title>
+    <?php $this->layout = 'ccsdt'; ?>
+    <meta http-equiv="refresh" content="300"><div id="title">
+    <h2 id="sdc"><center>CCSDT for DCSS v0.32</center></h2>
+    <h2>Week 3 &mdash; MfFi</h2></div><div id="content"><div id="times"><span class="label">Week of 2024-10-07</span></div><div id="combo"><span class="label">Character: </span>Merfolk Fighter</div>
+<div id="bonus"><span class="label">Tier I Bonus: </span>No bonus<br/>
+<span class="label">Tier II Bonus: </span>No bonus</div>
+<div id="gods"><span class="label">Gods: </span>Okawaru, Okawaru, Okawaru</div><div class="card"><table><tr class="head">
+    <th>Player</th>
+    <th>Unique Kill</th>
+    <th>Branch Enter</th>
+    <th>Branch End</th>
+    <th>Champion God</th>
+    <th>Collect a Rune</th>
+    <th>Collect 3 Runes</th>
+    <th>Obtain the Orb</th>
+    <th>Win</th>
+    <th>Bonus I</th>
+    <th>Bonus II</th>
+    <th>Total</th>
+    </tr></table></div></div>
+    <div id="bottomtext"><span class="menu"><a href="../ccsdt.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu">Week 3 - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updated: </span>2024-10-07 11:25 UTC</span></div>
