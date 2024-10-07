ce5ad15a66b8d15122cf3fd85053ab9c86e2747f
RoGGa <robert_gagne69@yahoo.com>
2024-10-07 07:54:24 -0400

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.32/1.html.php
+++ content/pages/ccsdt/0.32/1.html.php
@@ -18,5 +18,25 @@
     <th>Bonus I</th>
     <th>Bonus II</th>
     <th>Total</th>
-    </tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><a href="../ccsdt.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updated: </span>2024-10-07 11:25 UTC</span></div>
+    </tr><tr class="dead"><td class="name"><a href="https://cbro.berotato.org/morgue//Dumalacath/morgue-Dumalacath-20240923-232911.txt">Dumalacath</a></td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
+<tr class="dead"><td class="name"><a href="https://cbro.berotato.org/morgue//Naiya55/morgue-Naiya55-20240925-215740.txt">Naiya55</a></td><td class="pt">1</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">1</td></tr>
+<tr class="none"><td class="name">piedemon</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Blyxx</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Broken26</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">MauveAvengr</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">cfcfcfcfcf</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">TCC</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Colgate</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">HourglassMan</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Jannitor</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Strat</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">snackwell</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">TheMeInTeam</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">MysticSailboat</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGa</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">PigKeeper</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">goupaloupa</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">DocSulfur</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">person50605</td>
+                <td colspan="10"></td><td class="total">0</td></tr></table></div></div>
+    <div id="bottomtext"><span class="menu"><a href="../ccsdt.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu">Week 1 - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updated: </span>2024-10-07 11:54 UTC</span></div>
