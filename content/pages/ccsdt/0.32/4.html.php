c1d397f71a3ee01f606a9953d69bd360e84f54a9
RoGGa <robert_gagne69@yahoo.com>
2024-10-13 20:04:53 -0400

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.32/4.html.php
+++ content/pages/ccsdt/0.32/4.html.php
@@ -1,15 +1,54 @@
-<?php $this->layout = 'ccsdt'; ?>
-
-<h3>This week hasn't started yet!</h3>
-
-   <div id="bottomtext">
-     <span class="menu"><a href="ccsdt.html">Overview</a> - </span>
-     <span class="menu"><a href="about_ccsdt.html">Details</a> - </span>
-     <span class="menu"><a href="./0.32/standings.html">Standings</a></span>
-     <span class="menuspacer"></span>
-     <span class="menu"><a href="./0.32/1.html">Week 1</a> - </span>
-     <span class="menu"><a href="./0.32/2.html">Week 2</a> - </span>
-     <span class="menu"><a href="./0.32/3.html">Week 3</a> - </span>
-     <span class="menu"><a href="./0.32/4.html">Week 4</a> - </span>
-     <span class="menu"><a href="./0.32/5.html">Week 5</a></span>
-   </div>
+<title>Week 4 &mdash; DsIE</title>
+    <?php $this->layout = 'ccsdt'; ?>
+    <meta http-equiv="refresh" content="300"><div id="title">
+    <h2 id="sdc"><center>CCSDT for DCSS v0.32</center></h2>
+    <h2>Week 4 &mdash; DsIE</h2></div><div id="content"><div id="times"><span class="label">Week of 2024-10-14</span></div><div id="combo"><span class="label">Character: </span>Demonspawn Ice Elementalist</div>
+<div id="bonus"><span class="label">Bonus 1: </span>Reach the end of Elf before entering a rune branch.<br/>
+<span class="label">Bonus 2: </span>Kill or slimify Geryon before entering a rune branch (excluding the Abyss).</div>
+<div id="gods"><span class="label">Gods: </span>Sif Muna, Sif Muna, Sif Muna</div><div class="card"><table><tr class="head">
+    <th>Player</th>
+    <th>Unique Kill</th>
+    <th>Branch Enter</th>
+    <th>Branch End</th>
+    <th>Champion God</th>
+    <th>Collect a Rune</th>
+    <th>Collect 3 Runes</th>
+    <th>Obtain the Orb</th>
+    <th>Win</th>
+    <th>Bonus 1</th>
+    <th>Bonus 2</th>
+    <th>Total</th>
+    </tr><tr class="none"><td class="name">cfcfcfcfcf</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">rypofalem</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">TCC</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Colgate</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">HourglassMan</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">RepoMan</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Jannitor</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">petercordia2</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">petercordia3</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Strat</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">prawnwizard</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">agentchuck</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">snackwell</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">TheMeInTeam</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">MysticSailboat</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Dumalacath</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">shadowrider38</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Naiya55</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">grumposus</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">RoGGa</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">mauveavengr</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">testCosplay</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">PigKeeper</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">goupaloupa</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">DocSulfur</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">person50605</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Blyxx</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">abclol0070</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">piedemon</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Broken26</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">9to</td>
+                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">throwawayaccount</td>
+                <td colspan="10"></td><td class="total">0</td></tr></table></div></div>
+    <div id="bottomtext"><span class="menu"><a href="../ccsdt.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 10 mins. Last Updated: </span>2024-10-14 00:04 UTC</span></div>
