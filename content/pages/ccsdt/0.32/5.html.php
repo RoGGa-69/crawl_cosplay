fd6a7d2fa296f09ff7718cb19564678032b0e4a9
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-10-30 10:59:29 -0400

    Update 5.html.php

--- content/pages/ccsdt/0.32/5.html.php
+++ content/pages/ccsdt/0.32/5.html.php
@@ -1,6 +1,9 @@
 <?php $this->layout = 'ccsdt'; ?>
 <div id="title">
     <h2 id="sdc">Week 5 hasn't started yet</h2>
-</div>        
+</div>
+<p>The 2 bonuses will be:</p>
+<p> - "Enter a rune branch with all base skills < 11"; and </p>
+<p> - "Collect a rune in less than 15,000 turns."</p>
 <div id="bottomtext"><span class="menu"><a href="../ccsdt.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span>
     <span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu"><a href="5.html">Week 5</a></div>
