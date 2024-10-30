ebbfecc2058165bbbb369db6dcd9f8b8e0cd0556
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-10-30 10:49:26 -0400

    Update 1.html.php

--- content/pages/ccsdt/0.32/1.html.php
+++ content/pages/ccsdt/0.32/1.html.php
@@ -2,5 +2,8 @@
 <div id="title">
     <h2 id="sdc">Week 1 hasn't started yet.</h2>
 </div>
+<p>The 2 bonuses will be:</p>
+<p> - "Reach the end of Elf before entering a rune branch (excluding getting banished to the Abyss)"; and </p>
+<p> - "Kill or slimify Geryon before entering a rune branch (excluding the Abyss)."</p>
 <div id="bottomtext"><span class="menu"><a href="../ccsdt.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span>
     <span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu"><a href="5.html">Week 5</a></div>
