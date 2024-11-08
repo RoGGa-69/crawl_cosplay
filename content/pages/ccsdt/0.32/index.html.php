7f959f7553d2fdbd6b69d4418bc03258b1c51b78
RoGGa <robert_gagne69@yahoo.com>
2024-11-07 19:06:58 -0500

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.32/index.html.php
+++ content/pages/ccsdt/0.32/index.html.php
@@ -27,20 +27,13 @@ href="rules.html">rules page</a>.</li>
 
 <h2>Schedule</h2>
 
-<ul id="schedule"><li><span class="label"><a href="1.html">Week 1 &mdash; HuGl</a>:</span> 2024-11-01 to 2024-11-08<li><span class="label">Week 2:</span> 2024-11-08 to 2024-11-15<li><span class="label">Week 3:</span> 2024-11-15 to 2024-10-22<li><span class="label">Week 4:</span> 2024-11-22 to 2024-11-29<li><span class="label">Week 5:</span> 2024-11-29 to 2024-12-06</ul>
+<ul id="schedule"><li><span class="label"><a href="1.html">Week 1 &mdash; HuGl</a>:</span> 2024-11-01 to 2024-11-08<li><span class="label"><a href="2.html">Week 2 &mdash; TeAE</a>:</span> 2024-11-08 to 2024-11-15<li><span class="label">Week 3:</span> 2024-11-15 to 2024-10-22<li><span class="label">Week 4:</span> 2024-11-22 to 2024-11-29<li><span class="label">Week 5:</span> 2024-11-29 to 2024-12-06</ul>
 
-
-<h2>Sign Up</h2>
-
-<p>In order to sign up, set the first line of your 0.32 rcfile to</p> <pre
-id="rc"># csdc</pre><p>on <a
-href="https://crawl.develz.org/play.htm">any of the official online servers</a>
-before the end of the first week. Your name will appear in the standings once
-you've done this correctly and started at least one 0.32 game (though it may take about 20 minutes before it does).</p>
+<h2>Sign Up</h2> <p>Sign ups are now closed. See you in 0.33.</p>
 
 <h2>Credits</h2>
 
 <p>Original CSDC rules and organization by <a href="http://crawl.akrasiac.org/scoring/players/walkerboh.html">WalkerBoh</a> and 
 <a href="http://crawl.akrasiac.org/scoring/players/ebering.html">ebering</a>.
 Thank you to scrubbdaddy for python assistance. I am your host, RoGGa.</div>
-    <div id="bottomtext"><span class="menu"><a href="../ccsdt.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu">Week 2 - </span><span class="menu">Week 3 - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span>
+    <div id="bottomtext"><span class="menu"><a href="../ccsdt.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu">Week 3 - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span>
