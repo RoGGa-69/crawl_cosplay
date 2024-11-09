fe43ea19d01585c0f7be7b6e5fda998955eb01b4
RoGGa <robert_gagne69@yahoo.com>
2024-11-09 12:10:06 -0500

    Automated commit via ccsdt-cronjob.sh

--- content/pages/ccsdt/0.32/2.html.php
+++ content/pages/ccsdt/0.32/2.html.php
@@ -81,6 +81,7 @@
 <tr class="dead"><td class="name"><a href="http://crawl.xtahua.com/crawl/morgue/flestrin/morgue-flestrin-20241108-012704.txt">flestrin</a></td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">0</td></tr>
 <tr class="alive"><td class="name">chlorine</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">0</td></tr>
 <tr class="alive"><td class="name">quokkamancerX</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">0</td></tr>
+<tr class="alive"><td class="name">ereinion</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="pt">0</td><td class="total">0</td></tr>
 <tr class="none"><td class="name">cfcfcfcfcf</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">rypofalem</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">HourglassMan</td>
@@ -88,7 +89,6 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Jannitor</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Salticid</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">baljet</td>
-                <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">ereinion</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">P0WERM0DE</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">petercordia2</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">petercordia3</td>
@@ -169,4 +169,4 @@
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">mao</td>
                 <td colspan="10"></td><td class="total">0</td></tr><tr class="none"><td class="name">Freemason</td>
                 <td colspan="10"></td><td class="total">0</td></tr></table></div></div>
-    <div id="bottomtext"><span class="menu"><a href="../ccsdt.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu">Week 2 - </span><span class="menu">Week 3 - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 5 mins. Last Update: </span>2024-11-09 17:05 UTC</span></div>
+    <div id="bottomtext"><span class="menu"><a href="../ccsdt.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu">Week 2 - </span><span class="menu">Week 3 - </span><span class="menu">Week 4 - </span><span class="menu">Week 5 - </span><span id="updated"><span class="label">Updates every 5 mins. Last Update: </span>2024-11-09 17:10 UTC</span></div>
