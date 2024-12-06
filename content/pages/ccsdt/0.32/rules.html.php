14135a8bfe994f9af1dd3136116814a13ea3abb3
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-12-06 06:13:55 -0500

    Update rules.html.php

--- content/pages/ccsdt/0.32/rules.html.php
+++ content/pages/ccsdt/0.32/rules.html.php
@@ -1,32 +1,33 @@
-<title>Rules</title>
-    <?php $this->layout = 'ccsdt'; ?>
-    <div id="title">
-    <h2 id="sdc"><center>CCSDT for DCSS v0.32</center></h2>
-    <h2>Rules</h2></div><div id="content">
-    <ol>
-<li>Each challenge consists of playing a randomly chosen Crawl race/class combo
-(e.g. MiBe). The combo for each week of the competition will be announced at
-00:00 UTC on the Friday starting that week. All players have one week to
-finish a game using that combo. Only milestones recorded during the week (from
-00:00 UTC on the start date until 00:00 UTC on the end date) will count for
-scoring.</li>
-<li>Your first 0.32 game started on an official server during the week will count
+<?php $this->layout = 'ccsdt'; ?>
+<h2 style="color:rgb(69, 136, 5);">About Crawl Cosplay Sudden Death Tournament (CCSDT)</h2>
+
+<p>The Crawl Cosplay Sudden Death Tournament is a competition that aims to fill a
+Crawl niche not currently filled by the version release tournament or the other three Crawl Cosplays initiatives.
+The idea is for players to compete by trying to do as well as possible in a
+game of Crawl with one attempt only; if you die, that challenge is over (thus
+"sudden death"). This competition is a lower time commitment event that aims to challenge 
+players while playing one of the 100 or so Crawl Unique characters.</p>
+
+<h2>Rules</h2>
+<ol>
+<li>Each weekly challenge consists of playing a randomly chosen Crawl Unqiue's starting combo (e.g. Snorg, a TrBe). 
+The combos for each week of the competition have already been selected by our Crawl Cosplay @VIPs on our 
+<a href="https://discord.gg/WdbyURBcYp" target="_blank">Discord server<img src="/img/discord_transparent_border.png" width="18" height="18"></a>. 
+All players have one week to finish a game using that combo. Only milestones recorded during the week (from
+00:00 UTC on the start date until 00:00 UTC on the end date) will count for scoring.</li>
+<li>Your first 0.32 game started on an official server (except for LLD) during the week will count
 for scoring. This is the only allowed attempt, subject to rule 3 below.</li>
 <li>One redo per week is allowed if your first game ended in death with player
-XL < 5 (i.e., no redo once you hit XL 5). The redo game must be started after
-the end of the first game (no startscumming!).  The highest CSDC score of the
+XL < 5 (i.e., no redo once you reach XL 5). The redo game must be started after
+the end of the first game (no startscumming!).  The highest score of the
 two games is counted towards your score.</li>
-<li>Each challenge has 2 optional bonus challenges in addition to the
-race/class combo.</li>
-<li>Each challenge includes a list of gods. A point can be earned upon
+<li>Each challenge includes a list of 3 gods. A bonus point can be earned upon
 reaching ****** piety (or on worship with Gozag or Xom) with one of the listed
-gods. The point is lost if you ever abandon your god or are excommunicated. If
-the combo for the week is a zealot background, god points are only for sticking
-with the starting god. If the combo for the week is a demigod, the god point is automatically awarded.</li>
-<li>The season consists of 5 challenges total (i.e., 5 different combos). Each
-race and background will be selected at most once during the competition.</li>
+gods. The point is lost if you ever abandon your god or are excommunicated or worship another god first instead of the 3 choiced gods.</li>
+<li>The tournament consists of 5 challenges total (i.e., 5 different combos). Each
+race and background is selected at most once during the competition.</li>
 <li>The final rankings will be tallied at the end of week 5 and represent the
-final outcome. The player with the highest CSDC score is the champion.</li>
+final outcome. The player with the highest tournament score is the champion.</li>
 <li>Tiebreakers are (in order): number of weekly bonus points, highest in-game
 score.</li>
 <li>Don't play on more than one account. That misses the point.</li>
@@ -42,15 +43,16 @@ games achieve them.</p>
 week)</th><th></th></tr>
 <tr><td class="name">Kill a unique:</td><td class="pt">1</td></tr>
 <tr><td class="name">Enter a multi-level branch of the dungeon:</td><td class="pt">1</td></tr>
-<tr><td class="name">Reach the end of a multi-level branch (including D):</td><td class="pt">1</td></tr>
+<tr><td class="name">Reach the end of a multi-level branch (including Dungeon):</td><td class="pt">1</td></tr>
 <tr><td class="name">Champion a listed god:</td><td class="pt">1</td></tr>
 <tr><td class="name">Collect a rune:</td><td class="pt">1</td></tr>
 <tr><td class="name">Collect 3 or more runes:</td><td class="pt">1</td></tr>
-<tr><td class="name">Collect the Orb of Zot:</td><td class="pt">1</td></tr>
+<tr><td class="name">Pick up the Orb of Zot:</td><td class="pt">1</td></tr>
 <tr><td class="name">Win:</td><td class="pt">1</td></tr>
 <tr><td class="name">Complete Bonus 1:</td><td class="pt">1</td></tr>
 <tr><td class="name">Complete Bonus 2:</td><td class="pt">1</td></tr>
-<tr class="head" id="onetime"><th>One-time points (earned once during the tournament)</th><th></th></tr>
+<tr class="head" id="onetime"><th>One-time points (earned once during the
+tournament)</th><th></th></tr>
 <tr><td class="name">Win a game with 15 runes:</td><td class="pt">3</td></tr>
 <tr><td class="name">Clear a Ziggurat:</td><td class="pt">3</td></tr>
 <tr><td class="name">Enter Zot at XL 20 or lower:</td><td class="pt">3</td></tr>
@@ -59,8 +61,55 @@ week)</th><th></th></tr>
 <tr><td class="name">Get a rune without using potions or scrolls:</td><td class="pt">6</td></tr>
 </table>
 
-<p class="notes"> Unless specified, a bonus or one time point does not
-require you to win to earn the point.</p>
+<p class="notes"> It will not always be the case that earning a
+Bonus 2 implies that you have earned a Bonus 1; these points can be
+awarded separately. Unless specified, a bonus or one time point does not
+require you to win to earn the point(s).</p>
 
-</div>
-    <div id="bottomtext"><span class="menu"><a href="../ccsdt.html">Overview</a> - </span><span class="menu"><a href="../about_ccsdt.html">Details</a> - </span><span class="menu"><a href="standings.html">Standings</a></span><span class="menuspacer"></span><span class="menu"><a href="1.html">Week 1</a> - </span><span class="menu"><a href="2.html">Week 2</a> - </span><span class="menu"><a href="3.html">Week 3</a> - </span><span class="menu"><a href="4.html">Week 4</a> - </span><span class="menu"><a href="5.html">Week 5</a> - </span>
+<h2>List of possible Bonuses</h2>
+<h3>Bonus 1 list</h3>
+<ol>
+   <li><s>Enter a rune branch with all base skills < 11.</s></li>
+   <li>Enter Slime as your second multi-level branch (don't get banished).</li>
+   <li>Enter the Temple in less than 4,000 turns.</li>
+   <li><s>Reach the end of Lair at XL &leq; 12.</s></li>
+   <li><s>Reach the end of Elf before entering a rune branch (excluding getting banished to the Abyss).</s></li>
+   <li>Reach the end of the Depths before entering a rune branch (excluding getting banished to the Abyss).</li>
+   <li><s>Collect a rune before entering Shoals, Snake, Spider, or Swamp.</s></li>
+   <li><s>Collect a rune before entering Lair.</s></li>
+   <li>Collect a rune without dying (felids).</li>
+   <li>Kill a unique in bat form (vampires).</li>
+</ol>
+
+<h3>Bonus 2 list</h3>
+<ol>
+   <li><s>Collect the golden rune.</s></li>
+   <li>Collect a rune with all base skills < 11.</li>
+   <li>Get the slimy rune without entering any multi-level branch other than Lair, Slime, and Dungeon (don't get banished).</li>
+   <li><s>Collect a rune in less than 15,000 turns.</s></li>
+   <li>Reach the end of the Vaults at XL &leq; 18.</li>
+   <li><s>Kill or slimify Geryon before entering a rune branch (excluding the Abyss).</s></li>
+   <li>Get a rune from Hell or Pan before entering any other rune branch (excluding the Abyss).</li>
+   <li>Get a rune from Hell before entering any other rune branch (excluding the Abyss).</li>
+   <li><s>Collect at least 5 runes before entering the Depths.</s></li>
+   <li><s>Collect two runes without dying twice (felids).</s></li>
+   <li>Kill a Hell or Pan Lord unique in bat form (vampires).</li>
+</ol>
+
+<h2>Credits</h2>
+<p>Original <a href="https://csclub.uwaterloo.ca/~ebering/crawl/csdc/0.24/standings.html">CSDC</a> rules and organization by <a href="http://crawl.akrasiac.org/scoring/players/walkerboh.html">WalkerBoh</a> 
+and <a href="http://crawl.akrasiac.org/scoring/players/ebering.html">ebering</a>.
+<br>Thank you ScrubDaddy & grumposus for python assistance and PRs. 
+<br>I am your host, RoGGa.
+
+   <div id="bottomtext">
+     <span class="menu"><a href="index.html">Overview</a> - </span>
+     <span class="menu"><a href="rules.html">Details</a> - </span>
+     <span class="menu"><a href="standings.html">Standings</a></span>
+     <span class="menuspacer"></span>
+     <span class="menu"><a href="1.html">Week 1</a> - </span>
+     <span class="menu"><a href="2.html">Week 2</a> - </span>
+     <span class="menu"><a href="3.html">Week 3</a> - </span>
+     <span class="menu"><a href="4.html">Week 4</a> - </span>
+     <span class="menu"><a href="5.html">Week 5</a></span>
+   </div>
