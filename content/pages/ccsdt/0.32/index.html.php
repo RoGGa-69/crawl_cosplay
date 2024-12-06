108efeff319e00565b167e2e1c68ee54b2baf064
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-12-06 06:38:46 -0500

    Update index.html.php

--- content/pages/ccsdt/0.32/index.html.php
+++ content/pages/ccsdt/0.32/index.html.php
@@ -1,23 +1,22 @@
 <?php $this->layout = 'ccsdt'; ?>
-<h2 style="color:rgb(69, 136, 5);">Welcome to the <b>Crawl Cosplay Sudden Death Tournament</b> (CCSDT) for v0.32!</h2>
+<h2 style="color:rgb(69, 136, 5);">Welcome to the <b>Crawl Cosplay Sudden Death Tournament #1</b> (CCSDT) for DCSS v0.32!</h2>
 
 <h2>Competition Format</h2>
 <ul>
 <li>Each week's challenge consists of playing a Crawl Unique's starting combo.</li>
 <li>You get <em>one</em> attempt to play each combo. One redo per week is allowed if your first game ended in a death before XL 5.</li>
 <li>The goal is to advance as far as possible (and win!) in each game, scoring points by reaching various in-game milestones and bonuses along with one-time tournament challenges.</li>
-<li>Only games played and milestones scored between 00:00 UTC on the start and end dates count. 
-    To find out what time this is for your timezone, see: <a href="https://dateful.com/eventlink/2857668408">dateful.com/eventlink</a>.</li>
+<li>Only games played and milestones scored between 00:00 UTC on the start and end dates count.</li>
 </ul>
 
 <h2>Schedule</h2>
 
 <ul id="schedule">
-  <li><span class="label"><a href="0.32/1.html">Week 1</a> :</span> from 0:00 UTC on <b>2024-11-01</b> to 0:00 UTC on <b>2024-11-08</b> &mdash; Frances, a Human Gladiator (HuGl)
-  <li><span class="label"><a href="0.32/2.html">Week 2</a> :</span> from 0:00 UTC on <b>2024-11-08</b> to 0:00 UTC on <b>2024-11-15</b> &mdash; Sojobo, a Tengu Air Elementalist (TeAE)
-  <li><span class="label"><a href="0.32/3.html">Week 3</a> :</span> from 0:00 UTC on <b>2024-11-15</b> to 0:00 UTC on <b>2024-11-22</b> &mdash; The Enchantress, a Spriggan	Enchanter (SpEn)
-  <li><span class="label"><a href="0.32/4.html">Week 4</a> :</span> from 0:00 UTC on <b>2024-11-22</b> to 0:00 UTC on <b>2024-11-29</b> &mdash; Natasha, a Felid Necromancer (FeNe)
-  <li><span class="label"><a href="0.32/5.html">Week 5</a> :</span> from 0:00 UTC on <b>2024-11-29</b> to 0:00 UTC on <b>2024-12-06</b> &mdash; Amaemon, a Demonspawn	Alchemist (DsAl)
+  <li><span class="label"><a href="1.html">Week 1</a> :</span> from 0:00 UTC on <b>2024-11-01</b> to 0:00 UTC on <b>2024-11-08</b> &mdash; Frances, a Human Gladiator (HuGl)
+  <li><span class="label"><a href="2.html">Week 2</a> :</span> from 0:00 UTC on <b>2024-11-08</b> to 0:00 UTC on <b>2024-11-15</b> &mdash; Sojobo, a Tengu Air Elementalist (TeAE)
+  <li><span class="label"><a href="3.html">Week 3</a> :</span> from 0:00 UTC on <b>2024-11-15</b> to 0:00 UTC on <b>2024-11-22</b> &mdash; The Enchantress, a Spriggan	Enchanter (SpEn)
+  <li><span class="label"><a href="4.html">Week 4</a> :</span> from 0:00 UTC on <b>2024-11-22</b> to 0:00 UTC on <b>2024-11-29</b> &mdash; Natasha, a Felid Necromancer (FeNe)
+  <li><span class="label"><a href="5.html">Week 5</a> :</span> from 0:00 UTC on <b>2024-11-29</b> to 0:00 UTC on <b>2024-12-06</b> &mdash; Amaemon, a Demonspawn	Alchemist (DsAl)
 </ul>
 
 <h2>Signing Up</h2> 
