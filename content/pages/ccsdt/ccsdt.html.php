7f131583176431505d09d57c7fa92df9b70e5daa
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-12-06 06:36:12 -0500

    Update ccsdt.html.php

--- content/pages/ccsdt/ccsdt.html.php
+++ content/pages/ccsdt/ccsdt.html.php
@@ -1,42 +1,11 @@
 <?php $this->layout = 'ccsdt'; ?>
-<h2 style="color:rgb(69, 136, 5);">Welcome to the <b>Crawl Cosplay Sudden Death Tournament</b> (CCSDT) for v0.32!</h2>
+<h2 style="color:rgb(69, 136, 5);">Welcome to the <b>Crawl Cosplay Sudden Death Tournament</b> (CCSDT)</h2>
 
-<h2>Competition Format</h2>
+<h2>Past Tournaments</h2>
 <ul>
-<li>Each week's challenge consists of playing a Crawl Unique's starting combo.</li>
-<li>You get <em>one</em> attempt to play each combo. One redo per week is allowed if your first game ended in a death before XL 5.</li>
-<li>The goal is to advance as far as possible (and win!) in each game, scoring points by reaching various in-game milestones and bonuses along with one-time tournament challenges.</li>
-<li>Only games played and milestones scored between 00:00 UTC on the start and end dates count. 
-    To find out what time this is for your timezone, see: <a href="https://dateful.com/eventlink/2857668408">dateful.com/eventlink</a>.</li>
+    <li><a href="./0.32/index.html">CCSDT #1</a> for DCSS v0.32</li>
 </ul>
 
-<h2>Schedule</h2>
-
-<ul id="schedule">
-  <li><span class="label"><a href="0.32/1.html">Week 1</a> :</span> from 0:00 UTC on <b>2024-11-01</b> to 0:00 UTC on <b>2024-11-08</b> &mdash; Frances, a Human Gladiator (HuGl)
-  <li><span class="label"><a href="0.32/2.html">Week 2</a> :</span> from 0:00 UTC on <b>2024-11-08</b> to 0:00 UTC on <b>2024-11-15</b> &mdash; Sojobo, a Tengu Air Elementalist (TeAE)
-  <li><span class="label"><a href="0.32/3.html">Week 3</a> :</span> from 0:00 UTC on <b>2024-11-15</b> to 0:00 UTC on <b>2024-11-22</b> &mdash; The Enchantress, a Spriggan	Enchanter (SpEn)
-  <li><span class="label"><a href="0.32/4.html">Week 4</a> :</span> from 0:00 UTC on <b>2024-11-22</b> to 0:00 UTC on <b>2024-11-29</b> &mdash; Natasha, a Felid Necromancer (FeNe)
-  <li><span class="label"><a href="0.32/5.html">Week 5</a> :</span> from 0:00 UTC on <b>2024-11-29</b> to 0:00 UTC on <b>2024-12-06</b> &mdash; Amaemon, a Demonspawn	Alchemist (DsAl)
-</ul>
-
-<h2>Signing Up</h2> 
-<p>Sign up is closed.</p> 
-
-<h2>Standings</h2>
-<p>for: <a href="0.32/standings.html">v0.32</a></p>
-
-<h2>Rules, Scoring and Credits</h2>
+<h2>General Rules, Scoring and Credits</h2>
 See the <a href="about_ccsdt.html">CCSDT About page</a>.
 <br>
-   <div id="bottomtext">
-     <span class="menu"><a href="ccsdt.html">Overview</a> - </span>
-     <span class="menu"><a href="about_ccsdt.html">Details</a> - </span>
-     <span class="menu"><a href="./0.32/standings.html">Standings</a></span>
-     <span class="menuspacer"></span>
-     <span class="menu"><a href="./0.32/1.html">Week 1</a> - </span>
-     <span class="menu"><a href="./0.32/2.html">Week 2</a> - </span>
-     <span class="menu"><a href="./0.32/3.html">Week 3</a> - </span>
-     <span class="menu"><a href="./0.32/4.html">Week 4</a> - </span>
-     <span class="menu"><a href="./0.32/5.html">Week 5</a></span>
-   </div>
