4b164eaac860f40be7da85e32d07adce2417b6fc
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-12-06 07:02:25 -0500

    Update about_ccsdt.html.php

--- content/pages/ccsdt/about_ccsdt.html.php
+++ content/pages/ccsdt/about_ccsdt.html.php
@@ -141,15 +141,3 @@ require you to win to earn the point(s).</p>
 and <a href="http://crawl.akrasiac.org/scoring/players/ebering.html">ebering</a>.
 <br>Thank you ScrubDaddy & grumposus for python assistance and PRs. 
 <br>I am your host, RoGGa.
-
-   <div id="bottomtext">
-     <span class="menu"><a href="ccsdt.html">Overview</a> - </span>
-     <span class="menu"><a href="about_ccsdt.html">Details</a> - </span>
-     <span class="menu"><a href="./0.32/standings.html">Standings</a></span>
-     <span class="menuspacer"></span>
-     <span class="menu"><a href="./0.32/1.html">Week 1</a> - </span>
-     <span class="menu"><a href="./0.32/2.html">Week 2</a> - </span>
-     <span class="menu"><a href="./0.32/3.html">Week 3</a> - </span>
-     <span class="menu"><a href="./0.32/4.html">Week 4</a> - </span>
-     <span class="menu"><a href="./0.32/5.html">Week 5</a></span>
-   </div>
