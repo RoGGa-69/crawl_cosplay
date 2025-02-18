01b8498b82c67873549d3d7cdd6d2e49fefef709
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-18 12:15:08 -0500

    Update edit.html.php

--- content/pages/admin/challenges/edit.html.php
+++ content/pages/admin/challenges/edit.html.php
@@ -100,10 +100,10 @@ if ($data = $this->request->getPostData()) {
 		<br />
 		<br />
 		<label>
-			<span>Reddit thread</span> 
-			<br />(for the CCA challenges, this field is used for particleface's tutorial videos)
-			<br />(for the CCC challenges, this field is used for Dumalacath's videos)
-			<br />(for the CCTT challenges, this field is used to cross reference the Reddit's post announcing the challenge)<br />
+			<span>Multipurpose field orginally called: reddit</span> 
+			<br />- for the CCA challenges, this field is used for particleface's tutorial videos.
+			<br />- for the CCC challenges, this field is used for Dumalacath's videos.
+			<br />- for the CCTT challenges, this field is used to cross reference the Reddit's post announcing the challenge)<br />
 			<textarea name="reddit" cols="100" rows="3"><?=$cha->reddit?></textarea>
 		</label>
 		<br />
