dead437c3f1756f4d1b170eb8963ae29f43bb73b
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-18 07:24:45 -0500

    Update about_cctt.html.php

--- content/pages/cctt/about_cctt.html.php
+++ content/pages/cctt/about_cctt.html.php
@@ -16,14 +16,15 @@
     echo '<img src="/'.$dir_path."/".$filename1.'" alt="'.$filename1.'" width="72" height="72" style="float:right">';
 ?>
 
-<p>The <b>Crawl Cosplay Trunk Tournament</b> is a 4 to 5 week long tournament with a different challenge each week highlighting major Trunk changes since the previous stable version.
+<p>The <b>Crawl Cosplay Trunk Tournament</b> is a 5-week long tournament with a different challenge each week highlighting major Trunk changes since the previous stable version.
  It is based on the weekly <a href="http://cosplay.kelbi.org/" target="_blank">Crawl Cosplay Challenge</a> (CCC) series for <a href="http://crawl.develz.org/" target="_blank">Dungeon Crawl Stone Soup</a> (DCSS).</p>
 
-<p>Each week will feature a DCSS Unique character for you to play, with one or more options for starting species and background, as well as which god you must worship.
-  These are all mandatory and you must worship your god before entering any multi-level branch besides the dungeon (e.g. Lair, Orc, Depths). 
-  Also, you can't do anything to lose your religion, including abandoning your god. 
-  The goal is not to emulate the unique's behaviour exactly, but to get a good starting point on playing games with interesting requirements.</p>
-<p>In the event of a tie for top spot, a very difficult bonus challenge is envisioned.</p>
+<p>Each week will likely feature a DCSS Unique character for you to play, with one or more options for starting species and background, as well as which god you must worship.
+   These are all mandatory and you must worship your god before entering any multi-level branch besides the dungeon (e.g. Lair, Orc, Depths). 
+   Also, you can't do anything to lose your religion, including abandoning your god. 
+   The goal is not to emulate the unique's behaviour exactly, but to get a good starting point on playing games with interesting requirements.</p>
+<p>In the event of a tie for top spot, the last week's challenge will be very difficult challenge. 
+   The highest score for that last week will be used to determine the winner.</p>
 
 <p>The next tournament challenge will be posted Friday at 0:00 UTC.</p>
 
@@ -32,12 +33,13 @@
     echo '<img src="/'.$dir_path."/".$filename2.'" alt="'.$filename2.'" width="72" height="72" style="float:right">'; 
 ?>
 
-<p>The current challenge's details page has a "Submit a run" link.
- For those who haven't participated in CCC or CCTT before, just paste a link to your online morgue, and enter your playername and score in the comments. 
- Submissions will be approved by a moderator, who will verify your morgue and your score, and create a username for your future submissions.</p>
+<p>The current challenge's details page has a "Submit a CCTT run" link.
+    For those who haven't participated in CCC or CCTT before, just paste a link to your online morgue in the appropraite field,
+    and enter your playername and score in the comments. 
+    Submissions will be approved by a moderator, who will verify your morgue and your score, and create a username for your future submissions.</p>
 
 <h3>Restrictions for tournament play</h3>
-<p><b>- Submitted runs must be played in trunk on one of the <a href="https://crawl.develz.org/play.htm" target="_blank"> official online servers</a></b>.</p>
+<p><b>- Submitted runs must be played in trunk on one of the <a href="https://crawl.develz.org/play.htm" target="_blank"> official online servers</a></b> except for the Tokyo server (LLC) which only has the latest stable release available.</p>
 <p><b>- Seeded games are NOT accepted.</b></p>
 <p>- You can make as many attempts as you want.</p>
 <p>- Morgues must be submitted while a challenge is active. No late submissions!</p>
@@ -64,24 +66,25 @@
 <li>Win the game.</li></ul>
 
 <h4>Conducts</h4>
-<p>Each week will have three conducts for you to follow, that vary with every challenge.
+<p>Each week will have three optional conducts for you to follow, that vary with every challenge; some are quite easy while other will be more difficult.
   Following or completing each conduct is worth 5 points, to a maximum of half your score from milestones, rounded down.
-  (So if you achieve 3 milestones (15 points) you can earn up to 7 points from conduct bonuses, for a total of 22 points.)</p>
+  So if you achieve 3 milestones (15 points), you can earn up to 7 points from conduct bonuses, for a total of 22 points.</p>
 
 <h4>Bonus Challenges:</h4>
 <?php
     echo '<img src="/'.$dir_path."/".$filename4.'" alt="'.$filename4.'" width="72" height="72" style="float:right">'; 
 ?>
-<p>Each of the 4 challenges will also have two <em>optional</em> bonus challenges, that are usually difficult or unconventional.
+<p>Each of the 5 challenges will also have two <em>optional</em> bonus challenges, that are usually difficult or unconventional.
   Completing a bonus challenge doesn't affect your score, but gets you a <strong>star</strong>, a small trophy to recognize your skill.
-  Each week has one star that requires you to win the game, and another that doesn't. You don't need to follow any of the cosplay conducts to earn stars.</p>
+  Each week has one star that requires you to win the game or is to be done on the Orb rune, and another that doesn't. 
+  You don't need to follow any of the cosplay conducts to earn stars.</p>
 
 <h3>Credits</h3>
 
 <p>The Crawl Cosplay Trunk Tournament was created and is run by <a href="https://www.reddit.com/user/RoGGa_69/" target="_blank">RoGGa</a>
-  with the help from some of the Crawl Cosplay Challenge regulars.</p>
+  with the help from our Crawl Cosplay @VIPs in our CC Discord server.</p>
 
-<p>This CCTT part of the website was developed by <a href="https://github.com/alkemann" target="_blank">Alkemann</a>, with assistance from RoGGa.</p>
-<p>A big thank you to floraline for hosting CCC and the tournament on the old CKO server.</p>
+<p>The CCTT part of the website was developed by <a href="https://github.com/alkemann" target="_blank">Alkemann</a>, with assistance from RoGGa.</p>
+<p>A big thank you to floraline for hosting CC on the old CKO server.</p>
 
-<p>See the <a href="/ccc/about_ccc">CCC about page</a> for more detailed credits about the original website in 2019.</p>
+<p>See the <a href="/ccc/about_ccc">CCC about page</a> for more detailed credits about the original website live since 2019.</p>
