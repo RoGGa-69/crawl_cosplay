69ef4c0fe3b7da60ec98e35b26e32ba2b38e01b5
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-18 12:34:10 -0500

    Update edit.html.php

--- content/pages/admin/challenges/edit.html.php
+++ content/pages/admin/challenges/edit.html.php
@@ -104,7 +104,7 @@ if ($data = $this->request->getPostData()) {
 			<br />- for the CCA challenges, this field is used for particleface's tutorial videos.
 			<br />- for the CCC challenges, this field is used for Dumalacath's videos.
 			<br />- for the CCTT challenges, this field is used to cross reference the Reddit's post announcing the challenge)<br />
-			<textarea name="reddit" cols="100" rows="3"><?=$cha->reddit?></textarea>
+			<textarea name="reddit" cols="130" rows="3"><?=$cha->reddit?></textarea>
 		</label>
 		<br />
 		<br />
