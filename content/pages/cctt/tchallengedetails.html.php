725b1fb8a9c0a349b387e0b58b9619f0086060b2
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-19 08:15:59 -0500

    Update tchallengedetails.html.php

--- content/pages/cctt/tchallengedetails.html.php
+++ content/pages/cctt/tchallengedetails.html.php
@@ -45,8 +45,12 @@ $this->setData("meta", ['filename' => $cha->icon]);
 	<tr><th>Species</th><th>Background<th>Gods</th></tr>
 	<tr><td><?=$e($cha->species)?></td><td><?=$e($cha->background)?><td><?=$e($cha->gods)?></td></tr>
 </table>
-<p class="info">The Species, Background, and God choices are all mandatory. You must be worshipping one of the gods listed above before entering Lair, Orcish Mines, the Vaults or Depths, unless this isn't possible in which case you must worship them as soon as you can. Don't use faded altars (except in challenges where you can choose any god), and don't do anything to lose your religion unless otherwise specified.</p>
-<p class="info">NOTE: Except for Beogh, Ignis, Jiyva and Lugonu, an altar for the other gods will ALWAYS show up by D:10.</p>
+<p class="info">The Species, Background, and God choices are all mandatory. You must be worshipping one of the gods listed above before entering Lair, 
+	Orcish Mines, the Vaults or Depths, unless this isn't possible in which case you must worship them as soon as you can. 
+	Don't use faded altars (except in challenges where you can choose any god), and don't do anything to lose your religion unless otherwise specified.</p>
+<p class="info">IMPORTANT NOTES:<br>
+	- Except for Beogh, Ignis, Jiyva and Lugonu, an altar for the other gods will ALWAYS show up by D:10.<br>
+	- Small mistakes will likely NOT be forgiven unless they are minute. (If you are unsure please ask ASAP if you are forgiven in our CC Discord linked above.)</p>
 
 <?php if ($cha->special_rule) : ?>
 <h3>Special Rule or Notes</h3>
