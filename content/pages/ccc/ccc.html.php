11e2408c48661399265110946214b8a7aa84d6e3
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-20 10:09:04 -0500

    Update ccc.html.php

--- content/pages/ccc/ccc.html.php
+++ content/pages/ccc/ccc.html.php
@@ -14,7 +14,7 @@ if ($active) :
 	<?php if ($active->icon): ?><img src="<?=$e($active->icon)?>" class="head-icon" height="30px" /> <?php endif; ?>
 </h2>
 <p style="font-style: italic; color: #777;"><?=$e($active->description)?></p>
-<p style="text-align:right;">&#8627 <span style="font-size: smaller"><?=$e($active->shortform)?></span></b></p>
+<p style="text-align:right;">&#8627 <span style="font-size: smaller"><?=$e($active->shortform)?></span></p>
 
 <p><a href="/ccc/challengedetails?id=<?=$e($active->id)?>">Challenge details</a>  | <a href="/ccc/submit_ccc">Submit a CCC run</a> | <a href="https://discord.gg/ZQ4kk6n">Discuss it in CCC's Discord</a> | Next challenge starts on Monday.</p>
 <table class="table_for_layout">
