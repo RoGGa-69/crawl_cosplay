de9f25077b28eab5884dcbfb053652474ece5f51
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-20 07:15:06 -0500

    Update challengedetails.html.php

--- content/pages/ccc/challengedetails.html.php
+++ content/pages/ccc/challengedetails.html.php
@@ -45,7 +45,7 @@ $this->setData("meta", ['filename' => $cha->icon]);
 
 <?php if ($cha->icon) : ?>
 	<figure>
-		<img src="<?=$e($cha->icon)?> alt=$cha->shortform()" class="detail" />
+		<img src="<?=$e($cha->icon)?>" class="detail" />
 		<figcaption><span style="font-size: smaller"><?=$e($cha->shortform())?></span></figcaption>
 	</figure>
 <?php endif; ?>
