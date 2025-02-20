91508a993ddfa1dd6813cef6cd6eb01653d8d77c
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-20 08:39:44 -0500

    Update ccc.html.php

--- content/pages/ccc/ccc.html.php
+++ content/pages/ccc/ccc.html.php
@@ -41,7 +41,7 @@ if ($active) :
 	<tr>
 		<td>Week <?=$e($cha->week)?>.</td>
 		<td><?php if ($cha->icon):?><img src="<?=$e($cha->icon)?>" style="height: 1em" /><?php endif; ?> <b><a href="/ccc/challengedetails?id=<?=$e($cha->id)?>"><?=$e($cha->name)?></a></b></td>
-		<td><span style="font-size: smaller"><?=$e($cha->shortform())?></span></td>
+		<td><span style="font-size: smaller"><?=$e($cha->shortform)?></span></td>
 	</tr>
 <?php endforeach; ?>
 </table>
