c82bad162b31ec19f441e681dbd7efbcc54f4580
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-20 07:32:55 -0500

    Update challengedetails.html.php

--- content/pages/ccc/challengedetails.html.php
+++ content/pages/ccc/challengedetails.html.php
@@ -44,7 +44,7 @@ $this->setData("meta", ['filename' => $cha->icon]);
 <h2>Challenge Details</h2>
 
 <?php if ($cha->icon) : ?>
-	<div style="float:right;margin:0 0 6px 18px;padding:4px;text-align:center;border:1px solid #000000;">
+	<div style="float:right;margin:0 0 0px 0px; padding:0px; text-align:center; border:1px solid #000000;">
 	<figure>
 		<img src="<?=$e($cha->icon)?>" />
 		<figcaption><span style="font-size: smaller"><?=$e($cha->shortform)?></span></figcaption>
