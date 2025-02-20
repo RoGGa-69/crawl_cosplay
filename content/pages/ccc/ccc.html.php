e32c9160a0668b4bf683a675315af70a3010dacb
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-20 08:43:10 -0500

    Update ccc.html.php

--- content/pages/ccc/ccc.html.php
+++ content/pages/ccc/ccc.html.php
@@ -40,8 +40,10 @@ if ($active) :
 ?>
 	<tr>
 		<td>Week <?=$e($cha->week)?>.</td>
-		<td><?php if ($cha->icon):?><img src="<?=$e($cha->icon)?>" style="height: 1em" /><?php endif; ?> <b><a href="/ccc/challengedetails?id=<?=$e($cha->id)?>"><?=$e($cha->name)?></a></b></td>
-		<td><span style="font-size: smaller"><?=$e($cha->shortform)?></span></td>
+		<td><?php if ($cha->icon):?><img src="<?=$e($cha->icon)?>" style="height: 1em" /><?php endif; ?>
+		    <b><a href="/ccc/challengedetails?id=<?=$e($cha->id)?>"><?=$e($cha->name)?></a></b></td>
+		<td><span style="font-size: smaller"><?=($c->species), ", ", ($c->background), ", ", ($c->gods)?></td>
+		<td><?=$e($cha->shortform)?></span></td>
 	</tr>
 <?php endforeach; ?>
 </table>
