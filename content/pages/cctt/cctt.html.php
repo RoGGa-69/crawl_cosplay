27c2f03d490d16db3236698e25bf09ba0db7a636
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-20 10:11:30 -0500

    Update cctt.html.php

--- content/pages/cctt/cctt.html.php
+++ content/pages/cctt/cctt.html.php
@@ -19,6 +19,9 @@ if ($active) :
 	<?php if ($active->icon): ?><img src="<?=$e($active->icon)?>" class="head-icon" height="30px" /> <?php endif; ?>
 </h3>
 <p style="font-style: italic; color: #777;"><?=$e($active->description)?></p>
+/** shortform field is used for Monster Speak **/
+<p style="text-align:right;">&#8627 <span style="font-size: smaller"><?=$e($active->shortform)?></span></p>
+
 <p><a href="/cctt/tchallengedetails?id=<?=$e($active->id)?>">Challenge details</a>  
  | <a href="/cctt/submit_cctt?id=<?=$e($active->id)?>">Submit a CCTT run</a>
  | <a href="https://discord.gg/ZQ4kk6n">Discuss in our Discord</a> 
