f50cb9e01293df50482806a1ebf2fdeb76e472b5
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-20 10:12:52 -0500

    Update ccc.html.php

--- content/pages/ccc/ccc.html.php
+++ content/pages/ccc/ccc.html.php
@@ -15,7 +15,7 @@ if ($active) :
 </h2>
 <p style="font-style: italic; color: #777;"><?=$e($active->description)?></p>
 /** shortform field is used for Monster Speak **/
-<p style="text-align:right;">&#8627 <span style="font-size: smaller"><?=$e($active->shortform)?></span></p>
+<p style="text-align:right;"><span style="font-size: smaller"><?=$e($active->shortform)?></span></p>
 
 <p><a href="/ccc/challengedetails?id=<?=$e($active->id)?>">Challenge details</a>  | <a href="/ccc/submit_ccc">Submit a CCC run</a> | <a href="https://discord.gg/ZQ4kk6n">Discuss it in CCC's Discord</a> | Next challenge starts on Monday.</p>
 <table class="table_for_layout">
