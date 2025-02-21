363ca7dab37a64ef987bf96b21b5d8026d647873
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-21 12:51:48 -0500

    Update cctt.html.php

--- content/pages/cctt/cctt.html.php
+++ content/pages/cctt/cctt.html.php
@@ -24,8 +24,8 @@ if ($active) :
 
 <p><a href="/cctt/tchallengedetails?id=<?=$e($active->id)?>">Challenge details</a>  
  | <a href="/cctt/submit_cctt?id=<?=$e($active->id)?>">Submit a CCTT run</a>
- | <a href="https://discord.gg/ZQ4kk6n">Discuss in our Discord</a> 
- | <a href="<?=$e($active->reddit)?>">Reddit post</a>
+ | <a href="https://discord.gg/ZQ4kk6n" target="_blank">Discuss in our Discord</a> 
+ | <a href="<?=$e($active->reddit)?>" target="_blank">Reddit post</a>
  | Next challenge starts on Friday at 0:00 UTC.</p>
 <table class="table_for_layout">
 	<tr><th>Species</th><th>Background<th>Gods</th></tr>
