aa91988468b5a76755ada33789238958a3e0b07a
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-22 08:28:38 -0500

    Update ccsdt.html.php

--- content/pages/ccsdt/ccsdt.html.php
+++ content/pages/ccsdt/ccsdt.html.php
@@ -1,8 +1,7 @@
 <?php $this->layout = 'ccsdt'; ?>
 <h2 style="color:rgb(69, 136, 5);">Welcome to the <b>Crawl Cosplay Sudden Death Tournament</b> (CCSDT)</h2>
 
-<h2>Past Tournaments Results</h2>
-<p><a href="/ccsdt/past_ccsdt_results.html">CCSDT #1</a> for DCSS v0.32</p>
+<p><a href="/ccsdt/past_ccsdt_results.html">Past Tournaments Results</a>.</p>
 
 <h2>Next Tournament</h2>
 <p>Expected in mid 2025.</p>
