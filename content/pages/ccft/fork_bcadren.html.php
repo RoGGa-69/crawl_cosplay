9a79fb78ad15e3b8950132c96a5a471b43f0ae8f
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-23 07:41:46 -0500

    Update fork_bcadren.html.php

--- content/pages/ccft/fork_bcadren.html.php
+++ content/pages/ccft/fork_bcadren.html.php
@@ -1,21 +1,6 @@
-<?php
-    $this->layout = 'ccft';
+<?php     $this->layout = 'ccft'; ?>
 
-    $dir_path = "img/uniques";
-    $files = scandir($dir_path);
-    $count = count($files);
-    $index1 = rand(2, ($count-1));
-    $index2 = rand(2, ($count-1));
-    $index3 = rand(2, ($count-1));
-    $index4 = rand(2, ($count-1));
-    $filename1 = $files[$index1];
-    $filename2 = $files[$index2];
-    $filename3 = $files[$index3];
-    $filename4 = $files[$index4];
-
-    echo '<h2>About fork BCraden</h2>';
-    echo '<img src="/'.$dir_path."/".$filename1.'" alt="'.$filename1.'" width="72" height="72" style="float:right">';
-?>
+<h2>About fork BCraden</h2>
 
 <p>The <a href="http://github.com/Bcadren/crawl" target="_blank"><b>BCraden Fork</b></a> is full of changes even though it retains the food consumption aspect.</p>
 <p>For a text and graphical friendly writeup, see the README tab on <a href="https://github.com/Bcadren/crawl?tab=readme-ov-file" target="_blank">github.com/Bcadren/crawl</a></p>
