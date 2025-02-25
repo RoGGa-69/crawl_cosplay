b03a0315388f8d3a41532ab053ea980f2d8d7ccc
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-24 19:46:31 -0500

    Update neck.html.php

--- content/layouts/ccft/neck.html.php
+++ content/layouts/ccft/neck.html.php
@@ -19,6 +19,14 @@
 		</div>
 	</div>
 
+	<div class="dropdown" style="float:right;">
+		<button class="dropbtn">Donate!</button>
+		<div class="dropdown-content" style="left:0;">
+			The new webhosting cost is $11.30can/month.
+			Please donate at: <a href="https://patreon.com/rogga">Patreon</a>
+		</div>
+	</div>
+
     <div class="toplinks"><br></div>
     <div class="page_content">
         <div class="heading">
