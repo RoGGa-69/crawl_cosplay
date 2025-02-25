bde5c53dc59704b217038bfbf01945ce5a8b7067
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-25 08:05:26 -0500

    Update webhosting_details.html.php

--- content/pages/webhosting_details.html.php
+++ content/pages/webhosting_details.html.php
@@ -1,6 +1,8 @@
-<h2><b>Crawl Cosplay</b> webhosting monthly cost</h2>
-<ul><li>The future revamped crawlcosplay website is already under development as of Feb 24th 2025.</li>
+<h2><b>CrawlCosplay.org</b> webhosting monthly cost</h2>
+<ul><li>The future revamped crawlcosplay.org website is already under development as of Feb 24th 2025.</li>
     <li>The website is hosted at teksavvy.com at a monthly cost of $11.30can. That's really cheap!</li>
-    <li>The cost for Feb-March 2025 monthly billing period has already been covered by 4 gracious donours: 2 monthly memberships and 2 one-time donations. Thanks! :-D</li>
-    <li>Please consider getting a small monthly "membership" to cover the cost for subsequent months at: <a href="https://www.patreon.com/RoGGang" target="_blank">www.patreon.com/RoGGang</a></li>
+    <li>The cost for Feb-March 2025 monthly billing period has already been covered by 4 gracious donours: 
+        2 monthly memberships and 2 one-time donations. Thanks! :-D</li>
+    <li>Please consider getting a small monthly "membership" to cover the cost for subsequent months at:
+        <a href="https://www.patreon.com/RoGGa" target="_blank">www.patreon.com/RoGGa</a></li>
 </ul>
