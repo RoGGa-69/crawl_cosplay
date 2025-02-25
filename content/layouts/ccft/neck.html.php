d8376718deeb90f8dffcd8782b96549a02501d18
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-25 12:59:04 -0500

    Update neck.html.php

--- content/layouts/ccft/neck.html.php
+++ content/layouts/ccft/neck.html.php
@@ -1,9 +1,9 @@
-
 <div class="page information">
 	<div class="dropdown" style="float:left;">
 		<button class="dropbtn">Menu</button>
 		<div class="dropdown-content" style="left:0;">
 			<a href="/">Crawl Cosplay Home</a>
+			<a href="/cc_streamers"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; CC Streamers</a>
 			<a href="/cca/cca">Crawl Cosplay Academy (CCA)</a>
 			<a href="/cca/about_cca"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCA</a>
 			<a href="/ccc/ccc">Crawl Cosplay Challenge (CCC)</a>
