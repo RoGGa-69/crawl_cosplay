64a4c3634a42a214a6673062dad1504c9326ee87
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-25 12:45:38 -0500

    Create cc_streamers.html.php

---
+++ content/pages/cc_streamers.html.php
@@ -0,0 +1,22 @@
+<h2>Crawl Cosplay Streamers</h2>
+
+<p>Our Crawl Cosplay YouTube channel <a href="https://www.youtube.com/@dcsscrawlcosplayshorts" target="_blank">@dcsscrawlcosplayshorts</a> is repertoire of short videos 
+and shorts that have links to the full video or playlist for a Crawl Cosplay streamer.</p>
+
+<p>Please <b>Follow</b> / <b>Subscribe</b> to these streamers' channels, and <b>Like</b> their videos. It motivates them to make more great Crawl Cosplay content!</p>
+<ul>
+	<li><b>particleface</b>: CCA & CCTT <a href="https://www.twitch.tv/particleface" target="_blank">www.twitch.tv/particleface</a>
+		& <a href="https://www.youtube.com/@particleface" target="_blank">www.youtube.com/@particleface</a></li>
+	<li><b>Dumalacath</b>: CCA, CCC & CCTT <a href="https://www.youtube.com/@dumalacath" target="_blank">www.youtube.com/@dumalacath</a></li>
+	<li><b>Arcadio</b> (in french): CCTT <a href="https://www.twitch.tv/arcadiorl" target="_blank">www.twitch.tv/arcadiorl</a>
+		& <a href="https://www.youtube.com/@arcadiorl" target="_blank">www.youtube.com/@arcadiorl</a></li>
+</ul>
+<p>and the one and only:</p>
+<ul>	<li><b>MalcolmRose</b>: CCTT <a href="https://www.twitch.tv/malcolmrosegaming" target="_blank">www.twitch.tv/malcolmrosegaming</a>
+		& <a href="https://www.youtube.com/@MalcolmRoseGaming" target="_blank">www.youtube.com/@MalcolmRoseGaming</a></li>
+</ul>
+Of Note: 
+<ul><li>Malcolm Rose also has the longest streak ever in DCSS with over 75 wins and counting! So checkout his YouTube channel for past wins and hairy moments,
+	and Follow him on twitch.tv to be notified about his next World Record stream.</li>
+    <li>particleface is the most regular streamer of DCSS. You can catch him usually weekdays sometime between 9am and 1pm Eastern Timezone.</li>
+</ul>
