97f8fb600e9074c7febe6c5e2329d4990e620df8
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-02-25 14:33:01 -0500

    Update cc_streamers.html.php

--- content/pages/cc_streamers.html.php
+++ content/pages/cc_streamers.html.php
@@ -4,24 +4,38 @@
 	short videos and shorts that have links to the full video or playlist for a Crawl Cosplay streamer.</p>
 
 <p>Please <b>Follow</b> / <b>Subscribe</b> to these streamers' channels, and <b>Like</b> their videos. It motivates them to make more great Crawl Cosplay content!</p>
-<ul>
-	<li><b>particleface</b>:
-		  <a href="https://www.youtube.com/playlist?list=PLY2Drf0NBvk-TkLUf7T9Ceg_HtoPwOHtT" target="_blank"> CCA playlist
-		& CCTT 
-		& <a href="https://www.twitch.tv/particleface" target="_blank">www.twitch.tv/particleface</a>
-		& <a href="https://www.youtube.com/@particleface" target="_blank">www.youtube.com/@particleface</a></li>
-	<li><b>Dumalacath</b>: CCA, CCC & CCTT <a href="https://www.youtube.com/@dumalacath" target="_blank">www.youtube.com/@dumalacath</a></li>
-	<li><b>Arcadio</b> (in french): CCTT <a href="https://www.twitch.tv/arcadiorl" target="_blank">www.twitch.tv/arcadiorl</a>
-		& <a href="https://www.youtube.com/@arcadiorl" target="_blank">www.youtube.com/@arcadiorl</a></li>
+
+<p><b>particleface</b>:</p>
+<ul>	<li><a href="https://www.twitch.tv/particleface" target="_blank">www.twitch.tv/particleface</a></li>
+	<li><a href="https://www.youtube.com/@particleface" target="_blank">www.youtube.com/@particleface</a></li>
+	<li><a href="https://www.youtube.com/playlist?list=PLY2Drf0NBvk-TkLUf7T9Ceg_HtoPwOHtT" target="_blank">CCA playlist</a></li>
+	<li>CCTT playlist (coming soon).</li> 
+	<li>particleface is the most regular streamer of DCSS. You can catch him usually weekdays sometime between 9am and 1pm Eastern Timezone.</li>
+	<li>He has also been one of the few to complete !progress (winning with each 600+ DCSS combo.</li>
 </ul>
-<p>and the one and only:</p>
-<ul>	<li><b>MalcolmRose</b>: CCC & CCTT <a href="https://www.twitch.tv/malcolmrosegaming" target="_blank">www.twitch.tv/malcolmrosegaming</a>
-		& <a href="https://www.youtube.com/@MalcolmRoseGaming" target="_blank">www.youtube.com/@MalcolmRoseGaming</a>
-		& <a href="https://www.youtube.com/playlist?list=PLDaRV3E1eciYScSTw2RnztSrkVLNpbBAV" target="_blank">CCC playlist for v0.24</a></li>
+
+<p><b>Dumalacath</b>: The new blood on the block!</p>
+<ul>	<li><a href="https://www.youtube.com/@dumalacath" target="_blank">www.youtube.com/@dumalacath</a></li>
+	<li><a href="https://www.youtube.com/playlist?list=PLy8-17WgznzHupXlm9SbgNVWfeF46Yvku" target="_blank">CCA playlist</a></li>
+	<li><a href="https://www.youtube.com/playlist?list=PLy8-17WgznzGYSC6dJy6NqHErLrtvoYRR" target="_blank">CCC playlist</a></li>
+	<li><a href="https://www.youtube.com/playlist?list=PLy8-17WgznzHBfUlTlz_ScV5_irFeNx8C" target="_blank">CCTT playlist</a></li>
+</ul>
+
+<p><b>Arcadio</b> (in french):</p>
+<ul>	<li><a href="https://www.twitch.tv/arcadiorl" target="_blank">www.twitch.tv/arcadiorl</a></li>
+	<li><a href="https://www.youtube.com/@arcadiorl" target="_blank">www.youtube.com/@arcadiorl</a></li>
+	<li>CCTT playlist (coming soon)</li>
+</ul>
+
+<p><b>MalcolmRose</b>: the one and only!</p>
+<ul>	<li><a href="https://www.twitch.tv/malcolmrosegaming" target="_blank">www.twitch.tv/malcolmrosegaming</a></li>
+	<li><a href="https://www.youtube.com/@MalcolmRoseGaming" target="_blank">www.youtube.com/@MalcolmRoseGaming</a></li>
+	<li><a href="https://www.youtube.com/playlist?list=PLDaRV3E1eciYScSTw2RnztSrkVLNpbBAV" target="_blank">CCC playlist for v0.24</a></li>
+	<li>CCTT playlist (coming soon)</li>
+	<li>Malcolm Rose has the longest win-streak ever in DCSS with over 75 wins and counting! So checkout his YouTube channel for past wins and hairy moments,
+	and Follow him on twitch.tv to be notified about his next World Record stream.
+	Here's a link to his <a href="https://www.youtube.com/playlist?list=PLDaRV3E1ecibDeKDyvFUCYOKMPcO3FKLlDCSS" target="_blank">DCSS World Record playlist</a>.</li>
 </ul>
 Of Note: 
-<ul><li>Malcolm Rose also has the longest streak ever in DCSS with over 75 wins and counting! So checkout his YouTube channel for past wins and hairy moments,
-	and Follow him on twitch.tv to be notified about his next World Record stream.
-	Here's a link to his <a href="https://www.youtube.com/playlist?list=PLDaRV3E1ecibDeKDyvFUCYOKMPcO3FKLlDCSS" target="_blank">World Record playlist</a>.</li>
-    <li>particleface is the most regular streamer of DCSS. You can catch him usually weekdays sometime between 9am and 1pm Eastern Timezone.</li>
+<ul>
 </ul>
