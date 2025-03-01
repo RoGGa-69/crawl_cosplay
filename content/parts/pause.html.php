b83520696e0fc592af1831eb5d1dfbc6023c2e35
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-01 09:53:31 -0500

    Update pause.html.php

--- content/parts/pause.html.php
+++ content/parts/pause.html.php
@@ -1,9 +1,8 @@
 <?php /*
-
     This file is displayed when there are no active challenges
     alternative to the 'home' view.
-
 */ ?>
 
 <h3>No currently active challenges</h3>
-<p>Maybe take a look at <a href="/ccc/all_ccc_history">all CCC challenge</a>?</p>
+<p>This is likely due to the version release tournament for DCSS.</p>
+<p>For CCCs, take a look at <a href="/ccc/all_ccc_history">all CCC challenge</a>?</p>
