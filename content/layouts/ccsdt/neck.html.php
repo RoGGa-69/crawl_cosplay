1e4bf8f3b234ba5ece3ed12d0782fa0fcc320352
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-01 09:48:41 -0500

    Update neck.html.php

--- content/layouts/ccsdt/neck.html.php
+++ content/layouts/ccsdt/neck.html.php
@@ -8,19 +8,22 @@
 			<a href="/cca/about_cca"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCA</a>
 			<a href="/ccc/ccc">Crawl Cosplay Challenge (CCC)</a>
 			<a href="/ccc/about_ccc"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCC</a>
-			<a href="/ccc/all_ccc_history"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; All CCChallenges</a>
+			<a href="/ccc/all_ccc_history"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; all CCC challenges</a>
 			<a href="/cctt/cctt">Crawl Cosplay Trunk Tournament (CCTT)</a>
 			<a href="/cctt/about_cctt"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCTT</a>
-			<a href="/cctt/past_cctt_results"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Past CCTT Results</a>
+			<a href="/cctt/past_cctt_results"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Past CCTT results</a>
 			<a href="/ccsdt/ccsdt">Crawl Cosplay Sudden Death Tournament (CCSDT)</a>
 			<a href="/ccsdt/about_ccsdt"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCSDT</a>
+			<a href="/ccsdt/past_ccsdt_results.html"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Past CCSDT results</a>
+		        <a href="/ccft/ccft">Crawl Cosplay Fork Tournament (CCFT)</a>
+			<a href="/ccft/about_ccft"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCFT</a>
 		</div>
 	</div>
 
 	<div class="dropdown" style="float:right;">
 		<button class="dropbtn">Donate!</button>
 		<div class="dropdown-content" style="right:0;">
-			The new webhosting cost is $11.30can/month.<br>
+			The new webhosting cost is $11.30can/month...and we would like to get some more monthly "memberships"!<br>
 			Please donate at: <a href="https://patreon.com/rogga" target="_blank">Patreon</a>
 		</div>
 	</div>
