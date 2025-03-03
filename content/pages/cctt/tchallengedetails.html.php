779ceea960a76f0997bd8df2fa1796811efc17da
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-03 04:58:05 -0500

    Update tchallengedetails.html.php

--- content/pages/cctt/tchallengedetails.html.php
+++ content/pages/cctt/tchallengedetails.html.php
@@ -39,8 +39,8 @@ $this->setData("meta", ['filename' => $cha->icon]);
 		Submission Closed
 	<?php endif;?>
 	<?php if ($cha->wiki): ?> |
-	<a href="<?=$e($cha->wiki)?>">Wiki page</a><?php endif; ?>
-	| <a href="<?=$e($cha->reddit)?>">See Reddit post</a> <span style="font-size: smaller">with links to Twitch or YouTube videos.</span>
+	<a href="<?=$e($cha->wiki)?>" target="_blank">Wiki page</a><?php endif; ?>
+	| <a href="<?=$e($cha->reddit)?>" target="_blank">See Reddit post</a> <span style="font-size: smaller">with links to Twitch or YouTube videos.</span>
 </p>
 
 <?php if ($cha->icon) : ?><img src="<?=$e($cha->icon)?>" class="detail" /><?php endif; ?>
