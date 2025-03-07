c5ee3cced6df25664d99069d4217945e9a32e659
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-07 15:04:54 -0500

    Update cc_streamers.html.php

--- content/pages/cc_streamers.html.php
+++ content/pages/cc_streamers.html.php
@@ -49,7 +49,7 @@
 			<a href="https://www.youtube.com/@MalcolmRoseGaming" target="_blank">www.youtube.com/@MalcolmRoseGaming</a> & 
 			<a href="https://discord.gg/3khp7weR8j" target="_blank">MR moderated DCSS Discord server</a></li>
 	<li>Crawl Cosplay playlists: 	<a href="https://www.youtube.com/playlist?list=PLDaRV3E1eciYScSTw2RnztSrkVLNpbBAV" target="_blank">CCC for v0.24</a>; 
-					CCTT promo videos (coming soon)</li>
+					<a href="https://www.youtube.com/playlist?list=PLDaRV3E1eciYRRnD5qxmyjXCxL6sizmsQ" target="_blank">CCTT promo videos</a></li>
 	<li>Malcolm Rose has the longest win-streak ever in DCSS with over 75 wins and counting! So checkout his YouTube channel for past wins and hairy moments,
 	and Follow him on twitch.tv to be notified about his next World Record stream.
 	Here's a link to his <a href="https://www.youtube.com/playlist?list=PLDaRV3E1ecibDeKDyvFUCYOKMPcO3FKLlDCSS" target="_blank">DCSS World Record playlist</a>.</li>
