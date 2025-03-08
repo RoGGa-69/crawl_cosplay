99ab26199b018d41ee36baa14ba9211a149a629e
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-08 02:15:23 -0500

    Update projects.html.php

--- content/pages/projects.html.php
+++ content/pages/projects.html.php
@@ -1,6 +1,6 @@
 <h2>Crawl Cosplay projects in the works</h2>
-<p>The intent is to have a tansparent & light project management approach.</p>
-<ol><li>Launch a WebTiles.CrawlCosplay.org server ASAP and install all forks for CCForksTournament starting May 30th.
+<p>The intent is to have a tansparent & light project management approach visible by the DCSS community at large.</p>
+<ol><li>Launch a cco.CrawlCosplay.org server ASAP and install all forks for CCForksTournament starting May 30th.
         See: <a href="https://discord.com/channels/1344353790232363069/1346512455811072024" target="_blank">CC discord discussion thread</a>    </li>
     <li>Get DCSS on Stream &/or GoG.
         See: <a href="https://discord.com/channels/1344353790232363069/1346564348189081641" target="_blank">CC discord discussion thread</a>    </li> 
