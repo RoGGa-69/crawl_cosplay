a036627a199551eaa605d2b05d8266c45ad09374
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-08 09:10:53 -0500

    Create ccft.html.php

---
+++ content/pages/ccft.html.php
@@ -0,0 +1,11 @@
+<?php $this->layout = 'ccft'; ?>
+<h2 style="color:rgb(69, 136, 5);">Welcome to the <b>Crawl Cosplay Trunk Tournament</b> (CCFT)</h2>
+
+<!-- <p><a href="/ccft/past_ccft_results.html">Past CCFTournaments Results</a>.</p> -->
+
+<h2>Next Tournament</h2>
+<p>Expected in early June 2025.</p>
+
+<h2>General Rules, Scoring and Credits</h2>
+See the <a href="about_ccft.html">CCFT About page</a>.
+<br>
