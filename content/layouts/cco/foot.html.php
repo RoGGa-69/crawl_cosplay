3d911ac7d5eed7f23aaab80155e1e9b00d775fdf
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-09 01:01:10 -0500

    Create foot.html.php

---
+++ content/layouts/cco/foot.html.php
@@ -0,0 +1,11 @@
+		<br />
+		<img src="/img/HR-right.png"><br />
+		<br />
+		<footer>Copyleft  |  <a href="https://github.com/DCSS-CrawlCosplay-org/crawl_cosplay_org/issues" target="_blank">Send bug report or feature request</a></footer>
+		</div>
+	</div>
+</div>
+</div>
+<div id="page_footer">&nbsp;</div>
+</body>
+</html>
