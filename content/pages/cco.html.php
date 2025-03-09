c4619be34ea33b6ecd7361b157ee7c7d0c77f2ab
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-09 11:35:25 -0400

    Update cco.html.php

--- content/pages/cco.html.php
+++ content/pages/cco.html.php
@@ -1,10 +1,10 @@
 <?php $this->layout = 'cco'; ?>
 
-<h2>Welcome to the lobby for the <b>CCO WebTiles server</b>!</h2>
+<h2>Welcome to the homepage for the <b>CCO WebTiles server</b>!</h2>
 <pre><b><i>*** This is a mockup of the future CCO home webpage.</i></b> ***</pre>
 <h3><a href="/cco/lobby">CCO Lobby</a></h3>
 <h4><a href="/cco/code_of_conduct">CCO Code of Conduct</a></h4>
 
 <h3><a href="http://crawl.develz.org/wordpress/">DCSS Blog</a> for the latest news</h3>
-<h3><a href="http://crawl.akrasiac.org/docs/changelog.txt">Latest Changelog for Trunk</a> (aka Git)</h3>
+<h3><a href="http://crawl.akrasiac.org/docs/changelog.txt">Latest Changelog for Trunk</a> (aka git)</h3>
 <h3><a href="https://crawl.develz.org/tournament/0.32/">0.32 Tournament HomePage</a></h3>
