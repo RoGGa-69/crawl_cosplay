26773d4f09e1cb7c127f93acc8ae810c3b9862e2
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-09 11:02:22 -0400

    Update cco.html.php

--- content/pages/cco.html.php
+++ content/pages/cco.html.php
@@ -1,4 +1,6 @@
 <?php $this->layout = 'cco'; ?>
 
-<h2><a href="/cco/lobby">CCO Lobby</a></h2>
-<h3><a href="/cco/code_of_conduct">CCO Code of Conduct</a></h3>
+<h2>Welcome to the lobby for the <b>CCO WebTiles server</b>!</h2>
+<pre>This is a moockup of the future CCO lobby webpage.</pre>
+<h3><a href="/cco/lobby">CCO Lobby</a></h3>
+<h4><a href="/cco/code_of_conduct">CCO Code of Conduct</a></h4>
