a3c59ced247b4f99fde434588e1f201c75613d2a
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-08 19:08:35 -0500

    Create lobby.html.php

---
+++ content/pages/cco/lobby.html.php
@@ -0,0 +1,30 @@
+<h2>Welcome to cco.crawlcosplay.org (CCO) WebTiles! (morgues, ttyrecs)</h2>
+<p>By playing here you agree to our code of conduct.</p>
+<p>Users of CCO in violation of the Code of Conduct may have their accounts suspended temporarily or indefinitely (with little to no notice).</p>
+<br>
+<p>Admins may be contacted via the DCSS Crawl Cosplay 2.0 community discord server, or via email at: rogga @ crawlcosplay DOT org.</p>
+<br>
+<p>Crawl Blog</p>
+<p>Latest Changelog</p>
+<p>0.32 Tourney Page</p>
+<br>
+<p>The Crawl Cosplay Trunk Tournament (CCTT) is starting this Friday, Feb 21 @ 00:00 UTC.</p>
+<p>Check out the website above for details on participating.</p>
+<br>
+<br>
+<p>Hello, xxx!</p>
+<p>Games on this server are recorded for in-progress viewing and playback. Chat messages might be logged, depending on the player's settings.</p>
+
+<p>Play now:</p>
+<p>DCSS trunk (Updates Daily) (edit rc) | Trunk custom seed | Sprint trunk | Tutorial trunk</p>
+<p>DCSS 0.32 (Latest Version) (edit rc) | Custom seed 0.32 | Sprint 0.32 | Tutorial 0.32</p>
+DCSS 0.31 (edit rc) | Custom seed 0.31 | Sprint 0.31 | Tutorial 0.31</p>
+DCSS 0.30 (edit rc) | Custom seed 0.30 | Sprint 0.30 | Tutorial 0.30
+DCSS 0.29 (edit rc) | Custom seed 0.29 | Sprint 0.29 | Tutorial 0.29
+DCSS 0.28 (edit rc) | Custom seed 0.28 | Sprint 0.28 | Tutorial 0.28
+DCSS 0.27 (edit rc) | Custom seed 0.27 | Sprint 0.27 | Tutorial 0.27
+DCSS 0.26 (edit rc) | Custom seed 0.26 | Sprint 0.26 | Tutorial 0.26
+DCSS 0.25 (edit rc) | Custom seed 0.25 | Sprint 0.25 | Tutorial 0.25
+DCSS 0.24 (edit rc) | Custom seed 0.24 | Sprint 0.24 | Tutorial 0.24
+
+Games currently running:
