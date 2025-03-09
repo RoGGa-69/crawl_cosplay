63abf1c812a312f3077b8c10f36257009a0eb386
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-09 01:24:40 -0500

    Update neck.html.php

--- content/layouts/cco/neck.html.php
+++ content/layouts/cco/neck.html.php
@@ -3,7 +3,7 @@
     <div class="toplinks"><br></div>
     <div class="page_content">
         <div class="heading">
-            <h1><img src="/img/cco_stone_soup_icon-128x128.png" width="48" height="48" /> CCO.CrawlCosplay.org <img src="/img/cco_stone_soup_icon-128x128.png" width="48" height="48" /></h1>
+            <h1><img src="/img/cco_stone_soup_icon-512x512.png" width="48" height="48" /> CCO.CrawlCosplay.org <img src="/img/cco_stone_soup_icon-512x512.png" width="48" height="48" /></h1>
             <div class="fineprint"><center>Come chat with us on our <a href="https://discord.gg/pW7nqC8Wu3" target="_blank">Discord server<img src="/img/discord_transparent_border.png" width="18" height="18"></a></center></div>
         </div>
         <img src="/img/HR-right.png"><br>
