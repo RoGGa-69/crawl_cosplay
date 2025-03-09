4a9e73975f61966ccf83c4bf4b7da3f9cc150725
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-09 09:15:50 -0400

    Update cco.html.php

--- content/pages/cco.html.php
+++ content/pages/cco.html.php
@@ -1,4 +1,4 @@
 <?php $this->layout = 'cco'; ?>
 
-<a href="/cco/lobby">CCO Lobby</a>
-<a href="/cco/code_of_conduct">CCO Code of Conduct</a>
+<h2><a href="/cco/lobby">CCO Lobby</a></h2>
+<h3><a href="/cco/code_of_conduct">CCO Code of Conduct</a></h3>
