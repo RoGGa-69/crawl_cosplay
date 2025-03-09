d7a0da9137906b9309ade704a34ab74cc4d1a9c0
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-09 07:03:10 -0400

    Update head.html.php

--- content/layouts/cco/head.html.php
+++ content/layouts/cco/head.html.php
@@ -2,10 +2,10 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title><?=$page_title ?? 'Crawl Cosplay Academy (CCA)'?></title>
-    <link rel="icon" href="/img/feat-dg_entrance.png" type="image/png">
+    <title><?=$page_title ?? 'CCO Webtiles - CCO.CrawlCosplay.org'?></title>
+    <link rel="icon" href="/img/cco_stone_soup_icon-512x512.png" type="image/png">
     <!-- <link rel="stylesheet" 	href="/css/reset.css"> -->
-    <link rel="stylesheet" href="/css/cca.css?v=<?=time()?>">
+    <link rel="stylesheet" href="/css/cco.css?v=<?=time()?>">
     <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
 <style>
 
