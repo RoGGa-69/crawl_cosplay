97a81ccbce287048881b694bb8d23f796e79914c
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-09 08:22:34 -0400

    Create code_of_conduct.html.php

---
+++ content/pages/cco/code_of_conduct.html.php
@@ -0,0 +1,36 @@
+                     CCO Code of Conduct
+
+To help provide a safe and fun environment for everyone, please read this
+document and follow all rules while connected to this public DCSS server.
+
+Users that are found to be in violation of these rules may have their account
+suspended at any time, with no warning.
+
+
+1. Be courteous to other players. Harassment will not be tolerated.
+
+2. Don't use obscene account names.
+
+3. Don't use account names, or chat messages to send spam or
+   advertisements for unrelated content. Advertising your Dungeon Crawl content
+   is encouraged.
+
+4. Do not attempt to steal or break into user accounts that don't belong to
+   you.
+
+5. Abuse of the system is not acceptable. This includes the following:
+
+    * Running chat bots without prior consent from an administrator
+    * Engaging in any attack which degrades the performance of the server
+
+   These actions can negatively impact server performance and affect the
+   online experience for other players. Use good judgement to determine whether
+   something is abusive. If you aren't sure, contact an administrator first.
+   Abusive users will be banned without warning.
+
+6. Please do not use anonymizing services to evade bans, or to engage in abuse
+   or other unkind behavior.
+
+
+This code of conduct was written in good faith to preserve a fun atmosphere
+and a functioning server environment, so that all players can enjoy the game.
