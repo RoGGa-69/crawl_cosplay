004f2fef4eef6f4d8026965ee799a6719227f695
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-09 07:36:48 -0400

    Update neck.html.php

--- content/layouts/cco/neck.html.php
+++ content/layouts/cco/neck.html.php
@@ -1,6 +1,21 @@
 <div class="page information">
-    <?php echo $this->part('neck_dropdown_menus'); ?>
-    <div class="toplinks"><br></div>
+	<div class="dropdown" style="float:left;">
+		<button class="dropbtn">Menu</button>
+		<div class="dropdown-content" style="left:0;">
+			<a href="/">Crawl Cosplay Home</a>
+			<a href="/cco">CCO Home</a>
+			<a href="/cco/lobby"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; CCO webtiles server</a>
+			<a href="/cco/CCO webtiles server"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; CCO Code of Conduct</a>
+		</div>
+	</div>
+
+	<div class="dropdown" style="float:right;">
+		<button class="dropbtn">Donate!</button>
+		<div class="dropdown-content" style="right:0;">
+			The new webhosting cost is $11.30can/month...and we would like to get some more monthly "memberships"!<br>
+			Please donate at: <a href="https://patreon.com/rogga" target="_blank"><b style="color:yellow">Patreon</b></a>
+		</div>
+	</div>    <div class="toplinks"><br></div>
     <div class="page_content">
         <div class="heading">
             <h1><img src="/img/cco_stone_soup_icon-512x512.png" width="48" height="48" /> CCO.CrawlCosplay.org <img src="/img/cco_stone_soup_icon-512x512.png" width="48" height="48" /></h1>
