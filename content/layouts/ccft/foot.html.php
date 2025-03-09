0c95d60c9d418c66756f71dbc9431a4c207bf6fc
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-09 01:03:05 -0500

    Update foot.html.php

--- content/layouts/ccft/foot.html.php
+++ content/layouts/ccft/foot.html.php
@@ -1,7 +1,7 @@
 		<br />
 		<img src="/img/HR-right.png"><br />
 		<br />
-		<footer>Copyleft  |  <a href="https://github.com/RoGGa-69/crawl_cosplay/issues" target="_blank">Send bug report or feature request</a></footer>
+		<footer>Copyleft  |  <a href="https://github.com/DCSS-CrawlCosplay-org/crawl_cosplay_org/issues" target="_blank">Send bug report or feature request</a></footer>
 		</div>
 	</div>
 </div>
