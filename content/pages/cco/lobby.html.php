2b1ebc4321aaef5a343c977fc79905c228fe3600
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-09 11:45:03 -0400

    Update lobby.html.php

--- content/pages/cco/lobby.html.php
+++ content/pages/cco/lobby.html.php
@@ -15,8 +15,8 @@
    Users of CCO in violation of the Code of Conduct may have their accounts suspended temporarily or indefinitely (with little to no notice).</p>
 <p>Admins may be contacted via the DCSS Crawl Cosplay 2.0 community discord server, or via email at: <b>cco @ crawlcosplay . org</b></p>
 <br>
-<p>The <a href="/cctt">Crawl Cosplay Trunk Tournament</a> (CCTT) is started Friday, Feb 21 @ 00:00 UTC and runs until the end of March 27th @ 23:59.</p>
-<p>Check out the website above for details on participating.</p>
+<p>The <a href="/cctt">Crawl Cosplay Trunk Tournament</a> (CCTT) is started Friday, Feb 21 @ 00:00 UTC and runs until the end of March 27th @ 23:59.
+<br>Check out the website above for details on participating.</p>
 <br>
 <p>Hello, xxxxx!</p>
 <p>Games on this server are recorded for in-progress viewing and playback. Chat messages might be logged, depending on the player's settings.</p>
@@ -24,13 +24,13 @@
 <p>Play now:</p>
 <p>DCSS 0.32 (Latest Version) (edit rc) | Custom seed 0.32 | Sprint 0.32 | Tutorial 0.32</p>
 <p>DCSS trunk (Updates Daily) (edit rc) | Trunk custom seed | Sprint trunk | Tutorial trunk</p>
-DCSS 0.31 (edit rc) | Custom seed 0.31 | Sprint 0.31 | Tutorial 0.31</p>
-DCSS 0.30 (edit rc) | Custom seed 0.30 | Sprint 0.30 | Tutorial 0.30
-DCSS 0.29 (edit rc) | Custom seed 0.29 | Sprint 0.29 | Tutorial 0.29
-DCSS 0.28 (edit rc) | Custom seed 0.28 | Sprint 0.28 | Tutorial 0.28
-DCSS 0.27 (edit rc) | Custom seed 0.27 | Sprint 0.27 | Tutorial 0.27
-DCSS 0.26 (edit rc) | Custom seed 0.26 | Sprint 0.26 | Tutorial 0.26
-DCSS 0.25 (edit rc) | Custom seed 0.25 | Sprint 0.25 | Tutorial 0.25
-DCSS 0.24 (edit rc) | Custom seed 0.24 | Sprint 0.24 | Tutorial 0.24
+<p> DCSS 0.31 (edit rc) | Custom seed 0.31 | Sprint 0.31 | Tutorial 0.31<br>
+    DCSS 0.30 (edit rc) | Custom seed 0.30 | Sprint 0.30 | Tutorial 0.30<br>
+    DCSS 0.29 (edit rc) | Custom seed 0.29 | Sprint 0.29 | Tutorial 0.29<br>
+    DCSS 0.28 (edit rc) | Custom seed 0.28 | Sprint 0.28 | Tutorial 0.28<br>
+    DCSS 0.27 (edit rc) | Custom seed 0.27 | Sprint 0.27 | Tutorial 0.27<br>
+    DCSS 0.26 (edit rc) | Custom seed 0.26 | Sprint 0.26 | Tutorial 0.26<br>
+    DCSS 0.25 (edit rc) | Custom seed 0.25 | Sprint 0.25 | Tutorial 0.25<br>
+    DCSS 0.24 (edit rc) | Custom seed 0.24 | Sprint 0.24 | Tutorial 0.24</p>
 
 Games currently running:
