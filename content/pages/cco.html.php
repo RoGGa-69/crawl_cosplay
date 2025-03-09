22252dabe3b281f6283153248c0d718dbf560c76
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-09 11:41:26 -0400

    Update cco.html.php

--- content/pages/cco.html.php
+++ content/pages/cco.html.php
@@ -2,9 +2,15 @@
 
 <h2>Welcome to the homepage for the <b>CCO WebTiles server</b>!</h2>
 <pre><b><i>*** This is a mockup of the future CCO home webpage.</i></b> ***</pre>
-<h3><a href="/cco/lobby">CCO Lobby</a></h3>
-<h4><a href="/cco/code_of_conduct">CCO Code of Conduct</a></h4>
 
-<h3><a href="http://crawl.develz.org/wordpress/">DCSS Blog</a> for the latest news</h3>
-<h3><a href="http://crawl.akrasiac.org/docs/changelog.txt">Latest Changelog for Trunk</a> (aka git)</h3>
-<h3><a href="https://crawl.develz.org/tournament/0.32/">0.32 Tournament HomePage</a></h3>
+<h3><a href="/cco/lobby">CCO Lobby</a></h3>
+<p><a href="/cco/code_of_conduct">CCO Code of Conduct</a></p>
+
+<h3><a href="/">The www.CrawlCosplay.org website</a></h3>
+
+<h2><a href="/cco/lobby">All DCSS important links</a></h2>
+
+<h4><a href="http://crawl.develz.org/">Primary DCSS Website</a></h4>
+<h4><a href="http://crawl.develz.org/wordpress/">DCSS Blog</a> for the latest news</h4>
+<h4><a href="http://crawl.akrasiac.org/docs/changelog.txt">Latest Changelog for Trunk</a> (aka git)</h4>
+<h4><a href="https://crawl.develz.org/tournament/0.32/">v0.32 Tournament HomePage</a></h4>
