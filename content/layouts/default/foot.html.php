4e6cf00d66b09e2a95b41ce4ba886acb580553df
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-27 14:09:17 -0400

    Update foot.html.php

--- content/layouts/default/foot.html.php
+++ content/layouts/default/foot.html.php
@@ -1,7 +1,9 @@
 		<br />
 		<img src="/img/HR-right.png"><br />
 		<br />
-		<footer>Copyleft  |  <a href="https://github.com/DCSS-CrawlCosplay-org/crawl_cosplay_org/issues" target="_blank">Send bug report or feature request</a></footer>
+		<footer>Copyleft  |  <a href="https://github.com/DCSS-CrawlCosplay-org/crawl_cosplay_org/issues" target="_blank">Send bug report or feature request</a>
+		<p style="text-align:right;">You are too beserk; please calm down first!</p></footer>
+		
 		</div>
 	</div>
 </div>
