f72cafe668d1bddb9b98325637e019a62c14e728
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2025-03-27 19:36:03 -0400

    Update past_cctt_results.html.php

--- content/pages/cctt/past_cctt_results.html.php
+++ content/pages/cctt/past_cctt_results.html.php
@@ -2,4 +2,6 @@
 
 <h2>Past Crawl Cosplay Trunk Tournament (CCTT) results</h2>
 
-<ul><li><a href="/cctt/tresults.html?set=32">CCTT#1 results</a>.</li></ul>
+<ul><li><a href="/cctt/tresults.html?set=32">CCTT#1 results</a>.</li>
+    <li><a href="/cctt/tresults.html?set=33">CCTT#2 results</a>.</li>
+</ul>
